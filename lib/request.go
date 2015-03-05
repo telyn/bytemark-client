ca0a7dd6ed0aa77f5250e49c2a3255bdc1cdd277
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-03-05 13:24:56 +0000

    Refactor MakeRequest into Request & FireRequest, which are terrible ambiguous names

--- lib/request.go
+++ lib/request.go
@@ -8,10 +8,34 @@ import (
 	"strings"
 )
 
-// Make a request to the URL specified, giving the token stored in the auth.Client.
-// This is intended as the low-level work-horse of the libary
+// Make a request to the URL specified, giving the token stored in the auth.Client, returning the entirety of the response body.
+// This is intended as the low-level work-horse of the libary, but may be deprecated in favour of MakeRequest in order to use a streaming JSON parser.
+func (bigv *Client) Request(method string, location string, requestBody string) (responseBody []byte, err error) {
+	req, res, err := bigv.FireRequest(method, location, requestBody)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
 
-func (bigv *Client) MakeRequest(method string, location string, requestBody string) (responseBody string, err error) {
+	if bigv.DebugLevel > 1 {
+		fmt.Printf("%s: %s: %d\r\n", req.URL, res.StatusCode)
+	}
+
+	responseBody, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if bigv.DebugLevel > 2 {
+		fmt.Printf(string(responseBody))
+	}
+
+	return responseBody, nil
+}
+
+// Make an HTTP request and then request it, returning the request object, response object and any errors
+// For use by Client.MakeAndReadRequest, do not use externally except for testing
+func (bigv *Client) FireRequest(method string, location string, requestBody string) (req *http.Request, res *http.Response, err error) {
 	url := location
 
 	if strings.HasPrefix(location, "/") {
@@ -19,33 +43,18 @@ func (bigv *Client) MakeRequest(method string, location string, requestBody stri
 	}
 	cli := &http.Client{}
 
-	req, err := http.NewRequest(method, url, bytes.NewBufferString(requestBody))
+	req, err = http.NewRequest(method, url, bytes.NewBufferString(requestBody))
 	if err != nil {
-		return "", err
+		return req, nil, err
 	}
 
 	req.Header.Add("Accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+bigv.AuthSession.Token)
 
-	res, err := cli.Do(req)
+	res, err = cli.Do(req)
 	if err != nil {
-		return "", err
+		return req, res, err
 	}
-	defer res.Body.Close()
-
-	if bigv.DebugLevel > 1 {
-		fmt.Printf("%s: %s: %d\r\n", url, res.StatusCode)
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", err
-	}
-
-	if bigv.DebugLevel > 2 {
-		fmt.Printf(string(body))
-	}
-
-	return string(body), nil
+	return req, res, nil
 }
