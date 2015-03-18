efac51cbc271f32dfa92bfa86ba2187216aa8089
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-03-18 16:43:48 +0000

    Rename specs to types, fix them up a little

---
+++ lib/types.go
@@ -0,0 +1,97 @@
+package lib
+
+type Disk struct {
+	Label        string `json:"label"`
+	StorageGrade string `json:"storage_grade"`
+	Size         int    `json:"size"`
+
+	Id               int    `json:"id"`
+	VirtualMachineId int    `json:"virtual_machine_id"`
+	StoragePool      string `json:"storage_pool"`
+}
+
+type Image struct {
+	Distribution string `json:"distribution"`
+	RootPassword string `json:"root_password"`
+}
+
+type IP struct {
+	RDNS string `json:"rdns"`
+
+	// this cannot be set.
+	IP string `json:"ip"`
+}
+
+type NetworkInterface struct {
+	Label string `json:"label"`
+
+	Mac string `json:"mac"`
+
+	// the following can't be set (or at least, so I'm assuming..)
+
+	ID               int               `json:"id"`
+	VlanNum          int               `json:"vlan_num"`
+	IPs              []string          `json:"ips"`
+	ExtraIPs         map[string]string `json:"extra_ips"`
+	VirtualMachineId int               `json:"virtual_machine_id"`
+}
+
+type User struct {
+	Username       string `json:"username"`
+	Email          string `json:"email"`
+	AuthorizedKeys string `json:"authorized_keys"`
+	Password       string `json:"password"`
+
+	// "users can be created (using POST) without authentication. If the
+	// request has no authentication, it will also accept an account_name
+	// parameter and create an account at the same time."
+	AccountName string `json:"account_name"`
+}
+
+type VirtualMachine struct {
+	Autoreboot            bool   `json:"autoreboot_on"`
+	CdromURL              string `json:"cdrom_url"`
+	Cores                 int    `json:"cores"`
+	Memory                int    `json:"memory"`
+	Name                  string `json:"name"`
+	Power                 bool   `json:"power_on"`
+	HardwareProfile       string `json:"hardware_profile"`
+	HardwareProfileLocked bool   `json:"hardware_profile_locked"`
+	GroupId               int    `json:"group_id"`
+
+	// zone name can be set during creation but not changed
+	ZoneName string `json:"zone_name"`
+
+	// the following cannot be set
+	Discs             []*Disk             `json:"discs"`
+	Id                int                 `json:"id"`
+	ManagementAddress string              `json:"management_address"`
+	Deleted           bool                `json:"deleted"`
+	Hostname          string              `json:"hostname"`
+	Head              string              `json:"head"`
+	NetworkInterfaces []*NetworkInterface `json:"network_interfaces"`
+}
+
+type VirtualMachineSpec struct {
+	VirtualMachine VirtualMachine `json:"virtual_machine"`
+	Discs          []Disk         `json:"discs"`
+	Reimage        Image          `json:"reimage"`
+}
+
+type Group struct {
+	Name string `json:name"`
+
+	// the following cannot be set
+	AccountId       int              `json:"account_id"`
+	Id              int              `json:"id"`
+	VirtualMachines []VirtualMachine `json:"virtual_machines"`
+}
+
+type Account struct {
+	Name string `json:"name"`
+
+	// the following cannot be set
+	Id        int      `json:"id"`
+	Suspended bool     `json:"suspended"`
+	Groups    []*Group `json:"groups"`
+}
