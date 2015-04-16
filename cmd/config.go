telyn/bytemark-client

commit 9affa547929aebaae0db4bcc3393607d6619a399

Extract authenticating from the Dispatcher initialiser, sketch out Config.SetPersistent

cmd/config.go
@@ -113,8 +113,19 @@ func (config *Config) Get(name string) string {
 	return ""
 }
 
+// TODO(telyn): write a test for Set
+
+// Set stores the given key-value pair in config's Memo. This storage does not persist once the program terminates.
 func (config *Config) Set(name string, value string) {
 	config.Memo[name] = value
+}
+
+// TODO(telyn): write a test for SetPersistent
+
+// SetPersistent writes a file to the config directory for the given key-value pair.
+func (config *Config) SetPersistent(name, value string) {
+	config.Set(name, value)
+	// TODO(telyn): write a file here
 }
 
 func (config *Config) GetDefault(name string) string {

cmd/dispatcher.go
@@ -18,20 +18,26 @@ type Dispatcher struct {
 	BigV   *client.Client
 }
 
-// NewDispatcher creates a new Dispatcher given a config. It will check the auth for whatever token config can find, then try to grab credents from config, prompting if needed.
+// NewDispatcher creates a new Dispatcher given a config.
 func NewDispatcher(config *Config) (d *Dispatcher) {
 	d = new(Dispatcher)
 	d.Config = config
-	c, err := client.NewWithToken(config.Get("token"))
+	return d
+}
+
+// EnsureAuth makes sure a valid token is stored in config.
+// This should be called by anything that needs auth.
+func (d *Dispatcher) EnsureAuth() {
+	c, err := client.NewWithToken(d.Config.Get("token"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to use token, trying credentials.\r\n\r\n")
 		d.PromptForCredentials()
 		credents := map[string]string{
-			"username": config.Get("user"),
-			"password": config.Get("pass"),
+			"username": d.Config.Get("user"),
+			"password": d.Config.Get("pass"),
 		}
-		if config.Get("yubikey") != "" {
-			credents["yubikey"] = config.Get("yubikey-otp")
+		if d.Config.Get("yubikey") != "" {
+			credents["yubikey"] = d.Config.Get("yubikey-otp")
 		}
 
 		c, err = client.NewWithCredentials(credents)
@@ -40,36 +46,39 @@ func NewDispatcher(config *Config) (d *Dispatcher) {
 			panic(err)
 		}
 	}
-	debugLevel, err := strconv.ParseInt(config.Get("debug-level"), 10, 0)
+	debugLevel, err := strconv.ParseInt(d.Config.Get("debug-level"), 10, 0)
 	if err == nil {
 		c.DebugLevel = int(debugLevel)
 	}
 	d.BigV = c
-	return d
+
+	d.Config.SetPersistent("token", d.BigV.AuthSession.Token)
 }
 
 // PromptForCredentials ensures that user, pass and yubikey-otp are defined, by prompting the user for them.
-// needs more for loop to ensure that they don't stay empty.
+// needs a for loop to ensure that they don't stay empty.
 func (d *Dispatcher) PromptForCredentials() {
 	buf := bufio.NewReader(os.Stdin)
-	if d.Config.Get("user") == "" {
+	for d.Config.Get("user") == "" {
 		fmt.Fprintf(os.Stderr, "User: ")
 		user, _ := buf.ReadString('\n')
 		d.Config.Set("user", strings.TrimSpace(user))
 		fmt.Fprintf(os.Stderr, "\r\n")
 	}
 
-	if d.Config.Get("pass") == "" {
+	for d.Config.Get("pass") == "" {
 		fmt.Fprintf(os.Stderr, "Pass: ")
 		pass, _ := buf.ReadString('\n')
 		d.Config.Set("pass", strings.TrimSpace(pass))
 		fmt.Fprintf(os.Stderr, "\r\n")
 	}
 
-	if d.Config.Get("yubikey") != "" && d.Config.Get("yubikey-otp") == "" {
-		fmt.Fprintf(os.Stderr, "Press yubikey: ")
-		yubikey, _ := buf.ReadString('\n')
-		d.Config.Set("yubikey-otp", strings.TrimSpace(yubikey))
+	if d.Config.Get("yubikey") != "" {
+		for d.Config.Get("yubikey-otp") == "" {
+			fmt.Fprintf(os.Stderr, "Press yubikey: ")
+			yubikey, _ := buf.ReadString('\n')
+			d.Config.Set("yubikey-otp", strings.TrimSpace(yubikey))
+		}
 	}
 
 }

cmd/commands-debug.go
@@ -12,6 +12,7 @@ import (
 // command syntax: debug <method> <url>
 // URL probably needs to start with a /
 func (dispatch *Dispatcher) Debug(args []string) {
+	dispatch.EnsureAuth()
 	dispatch.BigV.DebugLevel = 1
 
 	// make sure the command is well-formed

cmd/commands-show.go
@@ -6,6 +6,8 @@ import (
 )
 
 func (dispatch *Dispatcher) ShowVM(args []string) {
+	dispatch.EnsureAuth()
+
 	name := ParseVirtualMachineName(args[0])
 
 	vm, err := dispatch.BigV.GetVirtualMachine(name)

commit 1067627e461bb7fe19232b68f2711e794455aaa3

Fix a bug which broke all useful functionality from 9affa547929

cmd/config.go
@@ -152,7 +152,7 @@ func (config *Config) Set(name string, value string) {
 // SetPersistent writes a file to the config directory for the given key-value pair.
 func (config *Config) SetPersistent(name, value string) {
 	config.Set(name, value)
-	err := ioutil.WriteFile(config.GetPath("user"), []byte(value), 0600)
+	err := ioutil.WriteFile(config.GetPath(name), []byte(value), 0600)
 	if err != nil {
 		fmt.Println("Couldn't write to config directory " + config.Dir)
 		panic(err)

cmd/dispatcher.go
@@ -83,11 +83,18 @@ func (d *Dispatcher) PromptForCredentials() {
 
 }
 
+// TODO(telyn): Write a test for Do. Somehow.
+
 // Do takes the command line arguments and figures out what to do
 func (dispatch *Dispatcher) Do(args []string) {
 	//	help := dispatch.Flags.Lookup("help")
 	///	fmt.Printf("%+v", help)
-	if dispatch.BigV.DebugLevel >= 1 {
+	debugLevel, err := strconv.ParseInt(dispatch.Config.Get("debug-level"), 10, 0)
+	if err != nil {
+		debugLevel = 0
+	}
+
+	if debugLevel >= 1 {
 		fmt.Fprintf(os.Stderr, "Args passed to Do: %#v\n", args)
 	}
 