16ad3bd26fb53fc03f06e720ba2b8d80b14c7caf
Telyn ZF Roat <telyn@aetheria.co.uk>
2015-05-25 21:05:08 +0200

    Add a mock bigv client

--- cmd/mocks_test.go
+++ cmd/mocks_test.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	bigv "bigv.io/client/lib"
+	auth3 "bytemark.co.uk/auth3/client"
+	"fmt"
 	mock "github.com/maraino/go-mock"
+	"net/http"
 )
+
+// mock Config
 
 type mockConfig struct {
 	mock.Mock
@@ -42,6 +48,8 @@ func (c *mockConfig) GetDebugLevel() int {
 	ret := c.Called()
 	return ret.Int(0)
 }
+
+// mock CommandSet
 
 type mockCommands struct {
 	mock.Mock
@@ -98,4 +106,73 @@ func (cmds *mockCommands) HelpForHelp() {
 
 func (cmds *mockCommands) HelpForShow() {
 	cmds.Called()
+}
+
+type mockBigVClient struct {
+	mock.Mock
+}
+
+func (c *mockBigVClient) GetEndpoint() string {
+	r := c.Called()
+	return r.String(0)
+}
+func (c *mockBigVClient) GetSessionToken() string {
+	r := c.Called()
+	return r.String(0)
+}
+func (c *mockBigVClient) SetDebugLevel(level int) {
+	c.Called(level)
+}
+func (c *mockBigVClient) AuthWithToken(token string) error {
+	r := c.Called(token)
+	return r.Error(0)
+}
+func (c *mockBigVClient) AuthWithCredentials(credents auth3.Credentials) error {
+	r := c.Called(credents)
+	return r.Error(0)
+}
+func (c *mockBigVClient) RequestAndUnmarshal(auth bool, method, path, requestBody string, output interface{}) error {
+	r := c.Called(auth, method, path, requestBody, output)
+	return r.Error(0)
+}
+func (c *mockBigVClient) RequestAndRead(auth bool, method, path, requestBody string) (responseBody []byte, err error) {
+	r := c.Called(auth, method, path, requestBody)
+	return r.Bytes(0), r.Error(1)
+}
+
+func (c *mockBigVClient) Request(auth bool, method string, location string, requestBody string) (req *http.Request, res *http.Response, err error) {
+	r := c.Called(auth, method, location, requestBody)
+	req, ok := r.Get(0).(*http.Request)
+	if !ok {
+		panic(fmt.Sprintf("Couldn't typecast request object because it was a %t", r.Get(0)))
+	}
+	res, ok = r.Get(1).(*http.Response)
+	if !ok {
+		panic(fmt.Sprintf("Couldn't typecast response object because it was a %t", r.Get(1)))
+	}
+	return req, res, r.Error(2)
+}
+
+func (c *mockBigVClient) GetAccount(name string) (account *bigv.Account, err error) {
+	r := c.Called(name)
+	acc, _ := r.Get(0).(*bigv.Account)
+	return acc, r.Error(1)
+}
+
+func (c *mockBigVClient) CreateGroup(name bigv.GroupName) error {
+	r := c.Called(name)
+	return r.Error(0)
+}
+func (c *mockBigVClient) DeleteVirtualMachine(name bigv.VirtualMachineName, purge bool) error {
+	r := c.Called(name, purge)
+	return r.Error(0)
+}
+func (c *mockBigVClient) GetVirtualMachine(name bigv.VirtualMachineName) (vm *bigv.VirtualMachine, err error) {
+	r := c.Called(name)
+	vm, _ = r.Get(0).(*bigv.VirtualMachine)
+	return vm, r.Error(1)
+}
+func (c *mockBigVClient) UndeleteVirtualMachine(name bigv.VirtualMachineName) error {
+	r := c.Called(name)
+	return r.Error(0)
 }
