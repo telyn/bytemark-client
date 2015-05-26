e4454e4320407f63cc6849b376878593b84252b3
Telyn ZF Roat <telyn@aetheria.co.uk>
2015-05-26 15:12:49 +0200

    Improved debug command: * --auth is now necessary to enable authentication * --junk-token wipes out the stored token * PUT and POSTs now read from stdin until eof.

--- main/cmd-debug.go
+++ main/cmd-debug.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 )
 
 // HelpForDebug outputs usage information for the debug command.
@@ -11,19 +14,31 @@ func (commands *CommandSet) HelpForDebug() {
 	fmt.Println("bigv debug")
 	fmt.Println()
 	fmt.Println("Usage:")
-	fmt.Println("    bigv debug GET <path>")
-	fmt.Println("    bigv debug DELETE <path>")
+	fmt.Println("    bigv debug [--junk-token] [--auth] GET <path>")
+	fmt.Println("    bigv debug [--junk-token] [--auth] DELETE <path>")
+	fmt.Println("    bigv debug [--junk-token] [--auth] PUT <path>")
+	fmt.Println("    bigv debug [--junk-token] [--auth] POST <path>")
 	fmt.Println()
-	fmt.Println("GET sends an HTTP GET request with a valid authorization header to the given path on the BigV endpoint and pretty-prints the received json.")
+	fmt.Println("GET sends an HTTP GET request with an optional valid authorization header to the given path on the BigV endpoint and pretty-prints the received json.")
+	fmt.Println("The rest do similar, but PUT and POST")
+	fmt.Println("The --junk-token flag sets the token to empty - useful if you want to ensure that credential-auth is working, or you want to do something as another user")
+	fmt.Println("The --auth token tells the client to gain valid auth and send the auth header on that request.")
 	fmt.Println()
 }
 
-// TODO(telyn): does the URL really have to start with /?
-
 // Debug makes an HTTP <method> request to the URL specified in the arguments.
 // command syntax: debug <method> <url>
-// URL probably needs to start with a /
 func (commands *CommandSet) Debug(args []string) {
+	flags := MakeCommonFlagSet()
+	junkToken := flags.Bool("junk-token", false, "")
+	shouldAuth := flags.Bool("auth", false, "")
+	flags.Parse(args)
+	args = commands.config.ImportFlags(flags)
+
+	if *junkToken {
+		commands.config.Set("token", "", "FLAG junk-token")
+	}
+
 	if len(args) < 1 {
 		commands.HelpForDebug()
 		return
@@ -31,11 +46,23 @@ func (commands *CommandSet) Debug(args []string) {
 
 	switch args[0] {
 	case "GET", "PUT", "POST", "DELETE":
-		// TODO(telyn): add a flag to disable auth
-		// TODO(telyn): add a flag to junk the token
-		shouldAuth := true
-		commands.EnsureAuth()
-		body, err := commands.bigv.RequestAndRead(shouldAuth, args[0], args[1], "")
+		if !strings.HasPrefix(args[1], "/") {
+			args[1] = "/" + args[1]
+		}
+		if *shouldAuth {
+			commands.EnsureAuth()
+		}
+
+		requestBody := ""
+		err := error(nil)
+		if args[0] == "PUT" || args[0] == "POST" {
+			buf := bufio.NewReader(os.Stdin)
+			requestBody, err = buf.ReadString(byte(uint8(14)))
+			if err != nil {
+				panic("Couldn't read from stdin")
+			}
+		}
+		body, err := commands.bigv.RequestAndRead(*shouldAuth, args[0], args[1], requestBody)
 		if err != nil {
 			exit(err)
 		}
