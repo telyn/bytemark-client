cb0d7d0cfbe88c944bc42f01c372f9aa23e12214
Telyn ZF Roat <telyn@aetheria.co.uk>
2015-05-26 22:02:09 +0200

    Add a todo for usage info

--- main/doc.go
+++ main/doc.go
@@ -4,3 +4,4 @@ Package main provides go-bigv, the command line interface to Bytemark's BigV ser
 package main
 
 // TODO(telyn): Change all instances of uk0 to api when we move to api.bigv.io - `make find-uk0` will help
+// TODO(telyn): Make usage info use $0 instead of 'go-bigv'
