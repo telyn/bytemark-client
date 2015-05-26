e6342e18942a87e375a30530d1d4ef796e4f2074
Telyn ZF Roat <telyn@aetheria.co.uk>
2015-05-26 12:12:24 +0200

    Fix package name on main/doc.go

--- main/doc.go
+++ main/doc.go
@@ -1,4 +1,4 @@
 /*
-Package client/cmd provides go-bigv, the command line interface to Bytemark's BigV service
+Package main provides go-bigv, the command line interface to Bytemark's BigV service
 */
 package main
