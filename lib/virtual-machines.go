02bc6e46fe0fc39a7192366efcc02d666159edbd
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-06-22 18:10:34 +0100

    Rearrange lib/virtual_machines.go. Alphabetical order nahmsayn

--- lib/virtual-machines.go
+++ lib/virtual-machines.go
@@ -6,39 +6,6 @@ var i2b = [...]string{
 	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 }
 
-// GetVirtualMachine requests an overview of the named VM, regardless of its deletion status.
-func (bigv *bigvClient) GetVirtualMachine(name VirtualMachineName) (vm *VirtualMachine, err error) {
-	vm = new(VirtualMachine)
-	path := BuildURL("/accounts/%s/groups/%s/virtual_machines/%s?include_deleted=true&view=overview", name.Account, name.Group, name.VirtualMachine)
-
-	err = bigv.RequestAndUnmarshal(true, "GET", path, "", vm)
-	if err != nil {
-		vm = nil
-	}
-	return vm, err
-}
-
-// DeleteVirtualMachine deletes the named virtual machine.
-// returns nil on success or an error otherwise.
-func (bigv *bigvClient) DeleteVirtualMachine(name VirtualMachineName, purge bool) (err error) {
-	path := BuildURL("/accounts/%s/groups/%s/virtual_machines/%s", name.Account, name.Group, name.VirtualMachine)
-	if purge {
-		path += "?purge=true"
-	}
-
-	_, _, err = bigv.Request(true, "DELETE", path, "")
-	return err
-}
-
-// UndeleteVirtualMachine changes the deleted flag on a VM back to false.
-// Return nil on success, an error otherwise.
-func (bigv *bigvClient) UndeleteVirtualMachine(name VirtualMachineName) (err error) {
-	path := BuildURL("/accounts/%s/groups/%s/virtual_machines/%s", name.Account, name.Group, name.VirtualMachine)
-
-	_, _, err = bigv.Request(true, "PUT", path, `{"deleted":false}`)
-	return err
-}
-
 //CreateVirtualMachine creates a virtual machine in the given group.
 func (bigv *bigvClient) CreateVirtualMachine(group GroupName, spec VirtualMachineSpec) (vm *VirtualMachine, err error) {
 	path := BuildURL("/accounts/%s/groups/%s/vm_create", group.Account, group.Group)
@@ -101,5 +68,29 @@ func (bigv *bigvClient) CreateVirtualMachine(group GroupName, spec VirtualMachin
 
 	vm = new(VirtualMachine)
 	err = bigv.RequestAndUnmarshal(true, "POST", path, string(js), vm)
+	return vm, err
+}
+
+// DeleteVirtualMachine deletes the named virtual machine.
+// returns nil on success or an error otherwise.
+func (bigv *bigvClient) DeleteVirtualMachine(name VirtualMachineName, purge bool) (err error) {
+	path := BuildURL("/accounts/%s/groups/%s/virtual_machines/%s", name.Account, name.Group, name.VirtualMachine)
+	if purge {
+		path += "?purge=true"
+	}
+
+	_, _, err = bigv.Request(true, "DELETE", path, "")
+	return err
+}
+
+// GetVirtualMachine requests an overview of the named VM, regardless of its deletion status.
+func (bigv *bigvClient) GetVirtualMachine(name VirtualMachineName) (vm *VirtualMachine, err error) {
+	vm = new(VirtualMachine)
+	path := BuildURL("/accounts/%s/groups/%s/virtual_machines/%s?include_deleted=true&view=overview", name.Account, name.Group, name.VirtualMachine)
+
+	err = bigv.RequestAndUnmarshal(true, "GET", path, "", vm)
+	if err != nil {
+		vm = nil
+	}
 	return vm, err
 }
