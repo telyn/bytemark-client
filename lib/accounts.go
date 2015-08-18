bd8392e1a19ba59edf31b4a19759d2a0bde73189
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-08-18 15:50:51 +0100

    Remove some debugging from lib/accounts

--- lib/accounts.go
+++ lib/accounts.go
@@ -1,12 +1,8 @@
 package lib
 
-import "fmt"
-
 // GetAccount takes an account name or ID and returns a filled-out Account object
 func (bigv *bigvClient) GetAccount(name string) (*Account, error) {
-	fmt.Println(name)
 	err := bigv.validateAccountName(&name)
-	fmt.Println(name)
 	if err != nil {
 		return nil, err
 	}
