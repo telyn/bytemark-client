42ae5e9cba965526182c38eaa94a5ee050562af9
Telyn ZF Roat <telyn.roat@aetheria.co.uk>
2015-09-07 13:13:35 +0100

    Fixed util.CallBrowser on windows

--- cmds/util/callbrowser_windows.go
+++ cmds/util/callbrowser_windows.go
@@ -4,20 +4,22 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 )
 
-func callBrowser(url string) error {
+func CallBrowser(url string) error {
 	fmt.Fprintf(os.Stderr, "Running a browser to open %s...\r\n", url)
 
 	var attr os.ProcAttr
-	attr.Sys = &syscall.SysProcAttr{HideWindow: true}
+	attr.Sys = &syscall.SysProcAttr{HideWindow: false}
+	attr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
 
 	path, err := exec.LookPath("cmd")
 	if err != nil {
 		return err
 	}
-	proc, err := os.StartProcess(path, []string{"start", url}, &attr)
+	proc, err := os.StartProcess(path, []string{path, "/C", "start", strings.Replace(url, "&", "^&", -1)}, &attr)
 	if err != nil {
 
 		return err
