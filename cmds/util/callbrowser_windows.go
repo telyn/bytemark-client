74b6ce801ce15a1a2b4df6aa51a3005a0c67ab07
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-09-08 07:34:01 +0100

    Set HideWindow: true for the cmd exe.

--- cmds/util/callbrowser_windows.go
+++ cmds/util/callbrowser_windows.go
@@ -12,7 +12,7 @@ func CallBrowser(url string) error {
 	fmt.Fprintf(os.Stderr, "Running a browser to open %s...\r\n", url)
 
 	var attr os.ProcAttr
-	attr.Sys = &syscall.SysProcAttr{HideWindow: false}
+	attr.Sys = &syscall.SysProcAttr{HideWindow: true}
 	attr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
 
 	path, err := exec.LookPath("cmd")
@@ -22,7 +22,7 @@ func CallBrowser(url string) error {
 	// so on windows when you're using cmd you have to escape ampersands with the ^ character.
 	// ¯\(º_o)/¯
 	url = strings.Replace(url, "&", "^&", -1)
-	proc, err := os.StartProcess(path, []string{path, "/C", "start", url }, &attr)
+	proc, err := os.StartProcess(path, []string{path, "/C", "start", url}, &attr)
 	if err != nil {
 
 		return err
