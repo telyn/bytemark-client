c6a6c719787825d20a7d638897ea6f6a2118149f
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-09-10 18:07:51 +0100

    Refactor tests

---
+++ cmds/lock_test.go
@@ -0,0 +1,62 @@
+package cmds
+
+import (
+	bigv "bigv.io/client/lib"
+	"bigv.io/client/mocks"
+	"testing"
+	//"github.com/cheekybits/is"
+)
+
+func TestLockHWProfileCommand(t *testing.T) {
+	c := &mocks.BigVClient{}
+	config := &mocks.Config{}
+
+	vmname := bigv.VirtualMachineName{
+		VirtualMachine: "test-vm",
+		Group:          "test-group",
+		Account:        "test-account"}
+	args := []string{"test-vm.test-group.test-account"}
+
+	config.When("Get", "token").Return("test-token")
+	config.When("Silent").Return(true)
+	config.When("ImportFlags").Return(args)
+
+	c.When("ParseVirtualMachineName", args[0]).Return(vmname).Times(1)
+	c.When("AuthWithToken", "test-token").Return(nil).Times(1)
+	c.When("SetVirtualMachineHardwareProfileLock", vmname, true).Return(nil).Times(1)
+	c.When("SetVirtualMachineHardwareProfileLock", vmname, false).Return(nil).Times(0)
+
+	cmds := NewCommandSet(config, c)
+	cmds.LockHWProfile(args)
+
+	if ok, err := c.Verify(); !ok {
+		t.Fatal(err)
+	}
+}
+
+func TestUnlockHWProfileCommand(t *testing.T) {
+	c := &mocks.BigVClient{}
+	config := &mocks.Config{}
+
+	vmname := bigv.VirtualMachineName{
+		VirtualMachine: "test-vm",
+		Group:          "test-group",
+		Account:        "test-account"}
+	args := []string{"test-vm.test-group.test-account"}
+
+	config.When("Get", "token").Return("test-token")
+	config.When("Silent").Return(true)
+	config.When("ImportFlags").Return(args)
+
+	c.When("ParseVirtualMachineName", args[0]).Return(vmname).Times(1)
+	c.When("AuthWithToken", "test-token").Return(nil).Times(1)
+	c.When("SetVirtualMachineHardwareProfileLock", vmname, true).Return(nil).Times(0)
+	c.When("SetVirtualMachineHardwareProfileLock", vmname, false).Return(nil).Times(1)
+
+	cmds := NewCommandSet(config, c)
+	cmds.UnlockHWProfile(args)
+
+	if ok, err := c.Verify(); !ok {
+		t.Fatal(err)
+	}
+}
---
+++ cmds/show_test.go
@@ -0,0 +1,51 @@
+package cmds
+
+import (
+	bigv "bigv.io/client/lib"
+	"bigv.io/client/mocks"
+	"testing"
+	//"github.com/cheekybits/is"
+)
+
+func TestShowGroupCommand(t *testing.T) {
+	c := &mocks.BigVClient{}
+	config := &mocks.Config{}
+
+	config.When("Get", "token").Return("test-token")
+	config.When("Silent").Return(true)
+	config.When("ImportFlags").Return([]string{"test-group.test-account"})
+
+	c.When("ParseGroupName", "test-group.test-account").Return(bigv.GroupName{Group: "test-group", Account: "test-account"})
+	c.When("AuthWithToken", "test-token").Return(nil).Times(1)
+
+	group := getFixtureGroup()
+	c.When("GetGroup", bigv.GroupName{Group: "test-group", Account: "test-account"}).Return(&group, nil).Times(1)
+
+	cmds := NewCommandSet(config, c)
+	cmds.ShowGroup([]string{"test-group.test-account"})
+
+	if ok, err := c.Verify(); !ok {
+		t.Fatal(err)
+	}
+}
+
+func TestShowVMCommand(t *testing.T) {
+	c := &mocks.BigVClient{}
+	config := &mocks.Config{}
+
+	config.When("Get", "token").Return("test-token")
+	config.When("Silent").Return(true)
+	config.When("ImportFlags").Return([]string{"test-vm.test-group.test-account"})
+
+	c.When("ParseVirtualMachineName", "test-vm.test-group.test-account").Return(bigv.VirtualMachineName{VirtualMachine: "test-vm", Group: "test-group", Account: "test-account"})
+	c.When("AuthWithToken", "test-token").Return(nil).Times(1)
+	vm := getFixtureVM()
+	c.When("GetVirtualMachine", bigv.VirtualMachineName{VirtualMachine: "test-vm", Group: "test-group", Account: "test-account"}).Return(&vm, nil).Times(1)
+
+	cmds := NewCommandSet(config, c)
+	cmds.ShowVM([]string{"test-vm.test-group.test-account"})
+
+	if ok, err := c.Verify(); !ok {
+		t.Fatal(err)
+	}
+}
