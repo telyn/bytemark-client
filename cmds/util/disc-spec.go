21057a88a478d06cf36139156bfe76526f075414
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-12-11 12:13:03 +0000

    Broaden disc spec to allow grade:size & size:grade Also improve error output for bad sizes.
    This commit fixes #13668

--- cmds/util/disc-spec.go
+++ cmds/util/disc-spec.go
@@ -19,6 +19,7 @@ func (e *DiscSpecError) Error() string {
 // ParseDiscSpec takes a disc spec and returns a slice of Discs (from bytemark.co.uk/client/lib)
 func ParseDiscSpec(spec string, trace bool) ([]bigv.Disc, error) {
 	// parser!
+	// this really needs to be rewritten with a lexer.
 	pos := 0
 
 	discs := make([]bigv.Disc, 0, 4)
@@ -32,6 +33,8 @@ func ParseDiscSpec(spec string, trace bool) ([]bigv.Disc, error) {
 	curDisc := 0
 	curGrade := ""
 	curSize := ""
+
+	sizePos := 0
 
 	expecting := _either
 
@@ -47,21 +50,18 @@ func ParseDiscSpec(spec string, trace bool) ([]bigv.Disc, error) {
 		}
 		switch expecting {
 		case _grade:
-			if c >= 'a' && c <= 'z' {
-				curGrade += spec[pos : pos+w]
-				pos += w
-			} else if c == ':' {
-				expecting = _size
-				pos += w
-			} else {
-				return nil, &DiscSpecError{pos, c}
-			}
-		case _size:
 			if c == ',' {
+				if len(curSize) == 0 {
+					return nil, &DiscSpecError{pos, c}
+				}
+
 				size, err := ParseSize(curSize)
 				if err != nil {
-					return discs, err
-					// this should logically be impossible - curSize should be a string solely containing characters from 0-9
+					if ssErr, ok := err.(*SizeSpecError); ok {
+						return nil, &DiscSpecError{ssErr.Position - sizePos, ssErr.Character}
+					} else {
+						return discs, err
+					}
 				}
 				discs = append(discs, bigv.Disc{
 					StorageGrade: curGrade,
@@ -73,15 +73,48 @@ func ParseDiscSpec(spec string, trace bool) ([]bigv.Disc, error) {
 				curDisc++
 				expecting = _either
 				pos += w
+			} else if c >= 'a' && c <= 'z' {
+				curGrade += spec[pos : pos+w]
+				pos += w
+			} else if c == ':' {
+				expecting = _size
+				pos += w
+				sizePos = pos
+			} else {
+				return nil, &DiscSpecError{pos, c}
+			}
+		case _size:
+			if c == ',' {
+				size, err := ParseSize(curSize)
+				if err != nil {
+					if ssErr, ok := err.(*SizeSpecError); ok {
+						return nil, &DiscSpecError{ssErr.Position - sizePos, ssErr.Character}
+					} else {
+						return discs, err
+					}
+				}
+				discs = append(discs, bigv.Disc{
+					StorageGrade: curGrade,
+					Size:         int(size),
+				})
+
+				curGrade = ""
+				curSize = ""
+				curDisc++
+				expecting = _either
+				pos += w
+			} else if c == ':' && curGrade == "" {
+				expecting = _grade
 			} else {
 				curSize += spec[pos : pos+w]
 				pos += w
 			}
-		default: // _expecting
+		default: // _either
 			if c >= 'a' && c <= 'z' {
 				expecting = _grade
 			} else if c >= '0' && c <= '9' {
 				expecting = _size
+				sizePos = pos
 			} else {
 				return nil, &DiscSpecError{pos, c}
 			}
--- cmds/util/disc-spec_test.go
+++ cmds/util/disc-spec_test.go
@@ -14,12 +14,19 @@ func TestParseDiscSpec(t *testing.T) {
 		test{"25", []bigv.Disc{bigv.Disc{StorageGrade: "", Size: 25 * 1024}}},
 		test{"10,32", []bigv.Disc{bigv.Disc{StorageGrade: "", Size: 10 * 1024}, bigv.Disc{StorageGrade: "", Size: 32 * 1024}}},
 		test{"15,archive:50", []bigv.Disc{bigv.Disc{StorageGrade: "", Size: 15 * 1024}, bigv.Disc{StorageGrade: "archive", Size: 50 * 1024}}},
+		test{"archive:200", []bigv.Disc{bigv.Disc{StorageGrade: "archive", Size: 200 * 1024}}},
+		test{"10M,11MiB,12MB,13G,14GB,15GiB", []bigv.Disc{bigv.Disc{Size: 10}, bigv.Disc{Size: 11}, bigv.Disc{Size: 12}, bigv.Disc{Size: 13 * 1024}, bigv.Disc{Size: 14 * 1024}, bigv.Disc{Size: 15 * 1024}}},
+		test{"archive:200M", []bigv.Disc{bigv.Disc{StorageGrade: "archive", Size: 200}}},
+		test{"archive:200Mib", []bigv.Disc{bigv.Disc{StorageGrade: "archive", Size: 200}}},
+		test{"archive:200mib", []bigv.Disc{bigv.Disc{StorageGrade: "archive", Size: 200}}},
+		test{"archive:200mb", []bigv.Disc{bigv.Disc{StorageGrade: "archive", Size: 200}}},
+		test{"archive:200MB", []bigv.Disc{bigv.Disc{StorageGrade: "archive", Size: 200}}},
 	}
 
 	for n, x := range tests {
 		r, err := ParseDiscSpec(x.Spec, true)
 		if err != nil {
-			t.Error(err)
+			t.Errorf("Test %d: %s", n, err.Error())
 			continue
 		}
 		if len(x.Discs) != len(r) {
