147afc804d9ae5172ec3f448f3f207c7ba03413f
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2015-12-11 16:55:17 +0000

    Add TestBadError

---
+++ lib/errors_test.go
@@ -0,0 +1,37 @@
+package lib
+
+import (
+	"testing"
+)
+
+func TestBadRequestError(t *testing.T) {
+	is := is.New(t)
+	type test struct {
+		input  string
+		output []string
+	}
+	tests := []test{
+		test{`{"discs":[{},{},{},{},{"size":["must be greater than or equal to 50"]}]}`,
+			[]string{
+				"• Disc 5 (vde) - size must be greater than or equal to 50",
+			},
+		},
+		test{`{"name":["can't be blank"],"memory":["is not included in the list","is not a number"]}`,
+			[]string{
+				"• Name cannot be blank",
+				"• Memory amount was not set",
+			},
+		},
+		test{`{"name":["is invalid","is too short (minimum is 3 characters)"],"memory":["is not included in the list","is not a number"]}`,
+			[]string{
+				"• Name is too short (minimum is 3 characters)",
+				"• Memory amount was not set",
+			},
+		},
+	}
+
+	for i, d := range tests {
+		err := NewBadRequestError(d.input)
+
+	}
+}
