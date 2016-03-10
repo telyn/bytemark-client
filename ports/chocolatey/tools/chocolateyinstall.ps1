6bbee6d147ec56128578633e7748ea38f7dfc78f
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-03-10 16:48:13 +0000

    Update chocolatey package's zip URL

--- ports/chocolatey/tools/chocolateyinstall.ps1
+++ ports/chocolatey/tools/chocolateyinstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyZipPackage 'bytemark-client' 'http://repo.bytemark.co.uk/bytemark-client/windows/bytemark-0.4.268.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿Install-ChocolateyZipPackage 'bytemark-client' 'http://repo.bytemark.co.uk/bytemark-client/windows/bytemark-0.5.0.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
