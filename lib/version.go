fa7a3777de67fb1e3ee4fe473bc4d31ebdc0127a
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-03-10 12:39:42 +0000

    Bump version

--- lib/version.go
+++ lib/version.go
@@ -1,11 +1,10 @@
 package lib
-
 const (
-	majorversion = 0
-	minorversion = 5
-	revision     = 0
-	buildnumber  = 0
-	gitcommit    = "unknown commit"
-	gitbranch    = "unknown branch"
 	builddate    = "unknown build date"
+  majorversion = 0
+  minorversion = 5
+  revision = 0
+  buildnumber = 0
+  gitcommit = "0a6c9e7898671b68fa17b0e8e2c4515f92afa175"
+  gitbranch = "master"
 )
