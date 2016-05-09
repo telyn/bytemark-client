e8c292c5764eb8ac8f48e9f391351e6d64bc140a
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-05-09 13:36:14 +0100

    Bump version to 0.7.0

--- ports/chocolatey/tools/chocolateyinstall.ps1
+++ ports/chocolatey/tools/chocolateyinstall.ps1
@@ -1,1 +1,1 @@
-﻿Install-ChocolateyZipPackage 'bytemark-client' 'http://repo.bytemark.co.uk/bytemark-client/windows/bytemark-0.6.0.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+﻿Install-ChocolateyZipPackage 'bytemark-client' 'http://repo.bytemark.co.uk/bytemark-client/windows/bytemark-0.7.0.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
