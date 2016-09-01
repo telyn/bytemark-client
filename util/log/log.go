4c4b081725b3a5cbb997d00982b29adbba2a07a2
telyn <telyn@bytemark.co.uk>
2016-09-01 12:06:02 +0100

    Document util/log

--- util/log/log.go
+++ util/log/log.go
@@ -18,9 +18,13 @@ const (
 	LvlMisc
 )
 
+// DebugLevel determines whether or not debugging output should be output to stderr.
 var DebugLevel int
+
+// LogFile is the file which bytemark-client is to log to. This can be nil, in which case it won't. Usually ~/.bytemark/debug.log
 var LogFile *os.File
 
+// Error outputs stuff to os.Stderr and LogFile, one thing per line.
 func Error(stuff ...interface{}) {
 	if len(stuff) == 0 {
 		Error("")
@@ -34,6 +38,7 @@ func Error(stuff ...interface{}) {
 
 }
 
+// Errorf formats the string and outputs it to Stderr and Logfile.
 func Errorf(format string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, format, args...)
 	if LogFile != nil {
@@ -41,6 +46,7 @@ func Errorf(format string, args ...interface{}) {
 	}
 }
 
+// Log outputs stuff to os.Stderr and LogFile, one thing per line.
 func Log(stuff ...interface{}) {
 	if len(stuff) == 0 {
 		Log("")
@@ -53,6 +59,7 @@ func Log(stuff ...interface{}) {
 	}
 }
 
+// Logf formats the string and outputs it to Stderr and Logfile.
 func Logf(format string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, format, args...)
 	if LogFile != nil {
@@ -60,6 +67,7 @@ func Logf(format string, args ...interface{}) {
 	}
 }
 
+// Output outputs stuff to os.Stdout and LogFile, one thing per line.
 func Output(stuff ...interface{}) {
 
 	if len(stuff) == 0 {
@@ -73,6 +81,7 @@ func Output(stuff ...interface{}) {
 	}
 }
 
+// Outputf formats the string and outputs it to Stdout and Logfile.
 func Outputf(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
 	if LogFile != nil {
@@ -80,6 +89,7 @@ func Outputf(format string, args ...interface{}) {
 	}
 }
 
+// Debug outputs stuff to LogFile, and to Stderr if DebugLevel >= level. One thing per line.
 func Debug(level int, stuff ...interface{}) {
 	for _, v := range stuff {
 		if level <= DebugLevel {
@@ -91,6 +101,7 @@ func Debug(level int, stuff ...interface{}) {
 	}
 }
 
+// Debugf formats the string and outputs it to LogFile, and to Stderr if DebugLevel >= level.
 func Debugf(level int, format string, args ...interface{}) {
 	if level <= DebugLevel {
 		fmt.Fprintf(os.Stderr, format, args...)
