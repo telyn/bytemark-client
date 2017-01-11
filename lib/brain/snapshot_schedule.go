2a568d778f19823861d2848ff51ee64138277084
telyn <telyn@bytemark.co.uk>
2017-01-11 15:19:40 +0000

    Add documentation comments to snapshot_schedule.go

--- lib/brain/snapshot_schedule.go
+++ lib/brain/snapshot_schedule.go
@@ -5,11 +5,14 @@ import (
 	"io"
 )
 
+// SnapshotSchedule represents a schedule to take snapshots on. It is represented as a start date in YYYY-MM-DD hh:mm:ss format (and assuming UK timezones of some kind.)
 type SnapshotSchedule struct {
 	StartDate string
 	Interval  int
 }
 
+// PrettyPrint outputs a nicely-formatted human-readable version of the schedule to the given writer.
+// All the detail levels are the same.
 func (sched SnapshotSchedule) PrettyPrint(wr io.Writer, detail prettyprint.DetailLevel) error {
 	scheduleTpl := `
 {{ define "schedule_sgl" }}{{ printf "Every %d seconds starting from %s" .Interval .StartDate }}{{ end }}
