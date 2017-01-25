e9c3e3cdecdd715ddfccd9757273f2d7e59d2dcf
telyn <telyn@bytemark.co.uk>
2017-01-25 10:37:47 +0000

    improve TestGrantPrivilege's structure

--- cmd/bytemark/grant_test.go
+++ cmd/bytemark/grant_test.go
@@ -3,41 +3,65 @@ package main
 import (
 	"github.com/BytemarkHosting/bytemark-client/lib"
 	"github.com/BytemarkHosting/bytemark-client/lib/brain"
+	"github.com/BytemarkHosting/bytemark-client/mocks"
 	"strings"
 	"testing"
 )
 
-func TestGrantPrivilege(t *testing.T) {
-	config, c := baseTestSetup(t, false)
+func setupGrantPrivilegeTest(t *testing.T) (config *mocks.Config, c *mocks.Client) {
+	config, c = baseTestSetup(t, false)
 
 	config.When("Get", "token").Return("test-token")
 	config.When("GetIgnoreErr", "yubikey").Return("")
 	config.When("GetIgnoreErr", "user").Return("test-user")
-	config.When("GetVirtualMachine").Return(&lib.VirtualMachineName{})
 
 	c.When("AuthWithToken", "test-token").Return(nil).Times(1)
-	c.When("ParseVirtualMachineName", "test-vm.test-group.test-account", []*lib.VirtualMachineName{{}}).Return(&lib.VirtualMachineName{
-		VirtualMachine: "test-vm",
-		Group:          "test-group",
-		Account:        "test-account",
-	}, nil)
-	c.When("GetVirtualMachine", &lib.VirtualMachineName{
-		VirtualMachine: "test-vm",
-		Group:          "test-group",
-		Account:        "test-account",
-	}).Return(&brain.VirtualMachine{ID: 333}, nil).Times(1)
+	return config, c
+}
 
-	c.When("GrantPrivilege", brain.Privilege{
-		Username:         "test-user",
-		Level:            brain.VMAdminPrivilege,
-		VirtualMachineID: 333,
-	}).Return(nil).Times(1)
+func TestGrantPrivilege(t *testing.T) {
 
-	err := global.App.Run(strings.Split("bytemark grant vm_admin on test-vm.test-group.test-account to test-user", " "))
-	if err != nil {
-		t.Error(err)
+	tests := []struct {
+		Setup     func(config *mocks.Config, c *mocks.Client)
+		ShouldErr bool
+		Input     string
+	}{
+		{
+			Setup: func(config *mocks.Config, c *mocks.Client) {
+				// specific to vm_admin/vm_console
+
+				config.When("GetVirtualMachine").Return(&defVM)
+				vm := lib.VirtualMachineName{
+					VirtualMachine: "test-vm",
+					Group:          "test-group",
+					Account:        "test-account",
+				}
+
+				c.When("ParseVirtualMachineName", "test-vm.test-group.test-account", []*lib.VirtualMachineName{{}}).Return(&vm, nil)
+				c.When("GetVirtualMachine", &vm).Return(&brain.VirtualMachine{ID: 333}, nil).Times(1)
+
+				c.When("GrantPrivilege", brain.Privilege{
+					Username:         "test-user",
+					Level:            brain.VMAdminPrivilege,
+					VirtualMachineID: 333,
+				}).Return(nil).Times(1)
+			},
+			ShouldErr: false,
+			Input:     "bytemark grant vm_admin on test-vm.test-group.test-account to test-user",
+		},
 	}
-	if ok, err := c.Verify(); !ok {
-		t.Fatal(err)
+	for i, test := range tests {
+		config, c := setupGrantPrivilegeTest(t)
+		test.Setup(config, c)
+
+		err := global.App.Run(strings.Split(test.Input, " "))
+		if test.ShouldErr && err == nil {
+			t.Errorf("TestGrantPrivilege %d should err and didn't", i)
+		} else if !test.ShouldErr && err != nil {
+			t.Errorf("TestGrantPrivilege %d shouldn't err, but: %s", i, err)
+		}
+		if ok, err := c.Verify(); !ok {
+			t.Fatal(err)
+		}
 	}
 }
