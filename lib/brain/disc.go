6b01e911bd8ee261302c6d56d3c027fd049371a6
telyn <telyn@bytemark.co.uk>
2017-02-07 10:42:36 +0000

    Add NewStorageGrade support to Disc

--- lib/brain/disc.go
+++ lib/brain/disc.go
@@ -14,13 +14,29 @@ type Disc struct {
 	ID               int    `json:"id,omitempty"`
 	VirtualMachineID int    `json:"virtual_machine_id,omitempty"`
 	StoragePool      string `json:"storage_pool,omitempty"`
+
+	BackupsEnabled  bool `json:"backups_enabled"`
+	BackupCount     int  `json:"backup_count"`
+	BackupSchedules BackupSchedules
+
+	NewStorageGrade string `json:"new_storage_grade,omitempty"`
+	NewStoragePool  string `json:"new_storage_pool,omitempty"`
 }
 
 // PrettyPrint outputs the disc nicely-formatted to the writer.
 func (d Disc) PrettyPrint(wr io.Writer, detail prettyprint.DetailLevel) error {
-	tmpl := `{{ define "disc_sgl" }}{{ .Label }} - {{ gibtib .Size }}, {{ .StorageGrade }} grade{{ end }}
+	tmpl := `{{ define "disc_sgl" }}{{ .Label }} - {{ gibtib .Size }}, {{ template "grade" . }}{{ end }}
 {{ define "disc_medium" }}{{ template "_sgl" . }}{{ end }}
-{{ define "disc_full" }}{{ template "_medium" . }}{{ end }}`
+{{ define "disc_full" }}{{ template "_medium" . }}{{ end }}
+{{ define "grade" }}
+  {{- if ne .NewStorageGrade "" -}}
+    {{- .NewStorageGrade }} grade (
+    {{- if eq .StorageGrade "iceberg" -}} restore {{- else -}} migration {{- end }} in progress)
+  {{- else -}}
+    {{- .StorageGrade }} grade
+  {{- end -}}
+{{- end }}
+`
 	return prettyprint.Run(wr, tmpl, "disc"+string(detail), d)
 }
 
