2bc3828d54fadbca4614f2ae50a3aef44a50a52f
telyn <telyn@bytemark.co.uk>
2017-02-10 10:21:08 +0000

    Fix schedule and unschedule commands for this refactor

--- cmd/bytemark/schedule.go
+++ cmd/bytemark/schedule.go
@@ -31,6 +31,15 @@ bytemark schedule backups --start "2017-04-05T14:37:00+02:00" fileserver very-im
 						Name:  "start",
 						Usage: "date & time the schedule starts. Assumes BST/GMT (depending on time of year) if not specified - defaults to 00:00",
 					},
+					cli.StringFlag{
+						Name:  "disc",
+						Usage: "the disc to schedule backups of",
+					},
+					cli.GenericFlag{
+						Name:  "server",
+						Usage: "the server the disc belongs to",
+						Value: new(VirtualMachineNameFlag),
+					},
 				},
 				Description: `schedule backups to occur at a regular interval (defined in seconds)
 		
@@ -41,7 +50,7 @@ bytemark schedule backups --start 00:00 fileserver very-important-data 86400
 
 To have hourly backups starting at 14:37 (Central European Summer Time) on the 5th of April, 2017:
 bytemark schedule backups --start "2017-04-05T14:37:00+02:00" fileserver very-important-data 3600`,
-				Action: With(VirtualMachineNameProvider, DiscLabelProvider, func(c *Context) (err error) {
+				Action: With(OptionalArgs("server", "disc"), func(c *Context) (err error) {
 					start := c.String("start")
 					if start == "" {
 						start = "00:00"
@@ -57,7 +66,8 @@ bytemark schedule backups --start "2017-04-05T14:37:00+02:00" fileserver very-im
 						return
 					}
 
-					_, err = global.Client.CreateBackupSchedule(*c.VirtualMachineName, *c.DiscLabel, start, interval)
+					vmName := c.VirtualMachineName("server")
+					_, err = global.Client.CreateBackupSchedule(vmName, c.String("disc"), start, interval)
 					if err == nil {
 						log.Log("Schedule set.")
 					}
--- cmd/bytemark/unschedule.go
+++ cmd/bytemark/unschedule.go
@@ -25,7 +25,18 @@ The <schedule id> is a number that can be found out using 'bytemark show disc <s
 	
 The <schedule id> is a number that can be found out using 'bytemark show disc <server> <disc>'
 `,
-				Action: With(VirtualMachineNameProvider, DiscLabelProvider, func(c *Context) (err error) {
+				Flags: []cli.Flag{
+					cli.GenericFlag{
+						Name:  "server",
+						Usage: "the server to unschedule backups on",
+						Value: new(VirtualMachineNameFlag),
+					},
+					cli.StringFlag{
+						Name:  "disc",
+						Usage: "the disc to unschedule backups of",
+					},
+				},
+				Action: With(OptionalArgs("server", "disc"), func(c *Context) (err error) {
 					idStr, err := c.NextArg()
 					if err != nil {
 						return
@@ -36,7 +47,8 @@ The <schedule id> is a number that can be found out using 'bytemark show disc <s
 						return
 					}
 
-					err = global.Client.DeleteBackupSchedule(*c.VirtualMachineName, *c.DiscLabel, id)
+					vmName := c.VirtualMachineName("server")
+					err = global.Client.DeleteBackupSchedule(vmName, c.String("disc"), id)
 					if err == nil {
 						log.Log("Backups unscheduled.")
 					}
