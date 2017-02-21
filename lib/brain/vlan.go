2f9b693af21d452cd93342a58f878d452d27ad7c
Phil Manavopoulos <phil.manavopoulos@bytemark.co.uk>
2017-02-21 11:24:58 +0000

    Comments to satisfy the linter

--- lib/brain/vlan.go
+++ lib/brain/vlan.go
@@ -2,12 +2,14 @@ package brain
 
 import "fmt"
 
+// VLAN is a representation of a VLAN, as used by admin endpoints
 type VLAN struct {
 	ID        int    `json:"id"`
 	Num       int    `json:"num"`
 	UsageType string `json:"usage_type"`
 }
 
+// String serialises a VLAN to easily be output
 func (v *VLAN) String() string {
 	return fmt.Sprintf("%d: %s (Num: %d)", v.ID, v.UsageType, v.ID)
 }
