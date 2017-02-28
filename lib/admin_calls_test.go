7381880f641e9a6b799230af73ad9fc73605087e
telyn <telyn@bytemark.co.uk>
2017-02-28 14:19:46 +0000

    Add TestGetIPRange

--- lib/admin_calls_test.go
+++ lib/admin_calls_test.go
@@ -89,3 +89,39 @@ func TestGetIPRanges(t *testing.T) {
 		t.Errorf("IPRanges returned from GetIPRanges were not what was expected.\r\nExpected: %#v\r\nActual:%#v", testIPRanges, ipranges)
 	}
 }
+
+func TestGetIPRange(t *testing.T) {
+	testIPRange := brain.IPRange{
+		ID:      1234,
+		Spec:    "192.168.13.0/24",
+		VLANNum: 123,
+		Zones: []string{
+			"test-zone",
+		},
+		Available: 200.0,
+	}
+	client, servers, err := mkTestClientAndServers(t, MuxHandlers{
+		brain: Mux{
+			"/admin/ip_ranges/1234": func(wr http.ResponseWriter, r *http.Request) {
+				assertMethod(t, r, "GET")
+				writeJSON(t, wr, testIPRange)
+			},
+		},
+	})
+	defer servers.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = client.AuthWithCredentials(map[string]string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	iprange, err := client.GetIPRange(1234)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(iprange, &testIPRange) {
+		t.Errorf("IPRange returned from GetIPRange was not what was expected.\r\nExpected: %#v\r\nActual:%#v", testIPRange, iprange)
+	}
+}
