60874dfdb0ff8df334a2dd6e26faa3d05172f5c7
telyn <telyn@bytemark.co.uk>
2017-05-12 13:38:05 +0100

    add comments for lib/brain/*Definition types

--- lib/brain/definitions.go
+++ lib/brain/definitions.go
@@ -1,19 +1,28 @@
 package brain
 
+// DistributionDefinition is an object we assemble from distributions and distribution_descriptions from the /definitions API call
+// in the future (bytemark-client 3.0?) a slice of these this will replace the Definitions.Distributions slice and Definitions.DistributionDescriptions map.
 type DistributionDefinition struct {
 	Name        string
 	Description string
 }
 
+// HardwareProfileDefinition is an object we assemble from hardwareprofiles in the /definitions API call and some static data in lib/definitions.go
+// in the future (bytemark-client 3.0?) a slice of these this will replace the Definitions.HardwareProfiles slice.
 type HardwareProfileDefinition struct {
 	Name        string
 	Description string
 }
 
+// StorageGradeDefinition is an object we assemble from storage_grades and storage_grade_descriptions in the /definitions API call
+// in the future (bytemark-client 3.0?) a slice of these this will replace the Definitions.StorageGrades slice and Definitions.StorageGradeDescriptions map.
 type StorageGradeDefinition struct {
 	Name        string
 	Description string
 }
+
+// ZoneDefinition is an object we assemble from zone_names in the /definitions API call and some static data in lib/definitions.go
+// in the future (bytemark-client 3.0?) a slice of these this will replace the Definitions.ZoneNames slice.
 type ZoneDefinition struct {
 	Name        string
 	Description string
