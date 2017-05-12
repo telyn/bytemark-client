6a48f308190acf1c014619cdda1f7bff0111207d
telyn <telyn@bytemark.co.uk>
2017-05-12 11:13:13 +0100

    Fix table and json for definitions commands (images, zones, storage, hwprofiles)

--- cmd/bytemark/definitions.go
+++ cmd/bytemark/definitions.go
@@ -14,7 +14,7 @@ func init() {
 Generally bytemark provide two - virtio and compatibility. The virtio one has better performance but may not work with obscure operating systems, or without drivers (particularly important if you are installing windows from CD rather than our images`,
 		Flags: OutputFlags("hardware profiles", "array"),
 		Action: With(DefinitionsProvider, func(c *Context) error {
-			return c.OutputInDesiredForm(c.Definitions.HardwareProfiles, func() error {
+			return c.OutputInDesiredForm(c.Definitions.HardwareProfileDefinitions(), func() error {
 				for _, profile := range c.Definitions.HardwareProfiles {
 					log.Log(profile)
 				}
@@ -29,7 +29,7 @@ Generally bytemark provide two - virtio and compatibility. The virtio one has be
 		Description: "This command lists all the images that are available for installation on Bytemark servers.",
 		Flags:       OutputFlags("images", "array"),
 		Action: With(DefinitionsProvider, func(c *Context) error {
-			return c.OutputInDesiredForm(c.Definitions.DistributionDescriptions, func() error {
+			return c.OutputInDesiredForm(c.Definitions.DistributionDefinitions(), func() error {
 				for _, distro := range c.Definitions.Distributions {
 					description := c.Definitions.DistributionDescriptions[distro]
 					log.Logf("'%s': %s\r\n", distro, description)
@@ -44,7 +44,7 @@ Generally bytemark provide two - virtio and compatibility. The virtio one has be
 		Description: "This outputs the available storage grades for cloud servers.",
 		Flags:       OutputFlags("storage grades", "array"),
 		Action: With(DefinitionsProvider, func(c *Context) error {
-			return c.OutputInDesiredForm(c.Definitions.StorageGradeDescriptions, func() error {
+			return c.OutputInDesiredForm(c.Definitions.StorageGradeDefinitions(), func() error {
 				for _, grade := range c.Definitions.StorageGrades {
 					description := c.Definitions.StorageGradeDescriptions[grade]
 					log.Logf("'%s': %s\r\n", grade, description)
@@ -59,7 +59,7 @@ Generally bytemark provide two - virtio and compatibility. The virtio one has be
 		Description: "This outputs the zones available for cloud servers to be stored and started in. Note that it is not currently possible to migrate a server between zones.",
 		Flags:       OutputFlags("zones", "array"),
 		Action: With(DefinitionsProvider, func(c *Context) error {
-			return c.OutputInDesiredForm(c.Definitions.ZoneNames, func() error {
+			return c.OutputInDesiredForm(c.Definitions.ZoneDefinitions(), func() error {
 				for _, zone := range c.Definitions.ZoneNames {
 					log.Log(zone)
 				}
---
+++ lib/brain/definitions.go
@@ -0,0 +1,20 @@
+package brain
+
+type DistributionDefinition struct {
+	Name        string
+	Description string
+}
+
+type HardwareProfileDefinition struct {
+	Name        string
+	Description string
+}
+
+type StorageGradeDefinition struct {
+	Name        string
+	Description string
+}
+type ZoneDefinition struct {
+	Name        string
+	Description string
+}
