f4b704df9445882e9eb414638b7fe20c7fe5f4f3
telyn <telyn@bytemark.co.uk>
2017-06-13 16:39:48 +0100

    Fix ScheduleBackups and UnscheduleBackups not defaulting correctly

--- lib/backup_schedule_calls.go
+++ lib/backup_schedule_calls.go
@@ -7,6 +7,10 @@ import (
 
 // CreateBackupSchedule creates a new backup schedule starting at the given date, with backups occuring every interval seconds
 func (c *bytemarkClient) CreateBackupSchedule(server VirtualMachineName, discLabel string, startDate string, interval int) (sched brain.BackupSchedule, err error) {
+	err = c.validateVirtualMachineName(&server)
+	if err != nil {
+		return
+	}
 	r, err := c.BuildRequest("POST", BrainEndpoint, "/accounts/%s/groups/%s/virtual_machines/%s/discs/%s/backup_schedules", server.Account, server.Group, server.VirtualMachine, discLabel)
 	if err != nil {
 		return
@@ -21,6 +25,10 @@ func (c *bytemarkClient) CreateBackupSchedule(server VirtualMachineName, discLab
 
 // DeleteBackupSchedule deletes the given backup schedule
 func (c *bytemarkClient) DeleteBackupSchedule(server VirtualMachineName, discLabel string, id int) (err error) {
+	err = c.validateVirtualMachineName(&server)
+	if err != nil {
+		return
+	}
 	r, err := c.BuildRequest("DELETE", BrainEndpoint, "/accounts/%s/groups/%s/virtual_machines/%s/discs/%s/backup_schedules/%s", server.Account, server.Group, server.VirtualMachine, discLabel, strconv.Itoa(id))
 	if err != nil {
 		return
