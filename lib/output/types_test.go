0b9ae55ee3956711ec78c8ee692d418418b38da5
telyn <telyn@bytemark.co.uk>
2017-08-02 15:17:47 +0100

    FIX EVERYTHING

--- lib/output/types_test.go
+++ lib/output/types_test.go
@@ -7,5 +7,5 @@ package output_test
 // if reflect ever gains support for listing all types in a package
 // then this can be rewritten into a single file in lib/output
 
-//go:generate go build "github.com/BytemarkHosting/bytemark-client/lib/brain" "github.com/BytemarkHosting/bytemark-client/lib/billing" "github.com/BytemarkHosting/bytemark-client/lib/spp"
+//go:generate go install "github.com/BytemarkHosting/bytemark-client/lib/brain" "github.com/BytemarkHosting/bytemark-client/lib/billing" "github.com/BytemarkHosting/bytemark-client/lib/spp"
 //go:generate go run ../../gen/list_types.go -f "(*%s)(nil)," -o generated_types_test.go -t types_test.go.inc "github.com/BytemarkHosting/bytemark-client/lib/brain" "github.com/BytemarkHosting/bytemark-client/lib/billing" "github.com/BytemarkHosting/bytemark-client/lib/spp"
