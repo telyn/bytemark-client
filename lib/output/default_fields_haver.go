76d8e4d2c9fd0ce957a253ea2a972b9423938301
telyn <telyn@bytemark.co.uk>
2017-08-04 11:08:28 +0100

    Add documentation comment to DefaultFieldsHaver

--- lib/output/default_fields_haver.go
+++ lib/output/default_fields_haver.go
@@ -1,5 +1,8 @@
 package output
 
+// DefaultFieldsHaver is an interface that must be implemented in order to pass objects to output.Write
 type DefaultFieldsHaver interface {
+	// DefaultFields must return a string of valid field names (according to github.com/BytemarkHosting/row - i.e. they must be in the list output by row.FieldsFrom) for the type it is implemented on.
+	// It is used to discover what fields should be output by output.Write when there's no list of user-specified fields.
 	DefaultFields(f Format) string
 }
