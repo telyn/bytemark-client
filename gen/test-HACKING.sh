18dd08f67c6fc52a5d6db615ca9db4ff3f1852b5
telyn <telyn@bytemark.co.uk>
2017-09-18 14:24:55 +0100

    Update HACKING with the actual sha256sum now that I can reproduce it on my laptop.
    My issue was that I had empty directories.

--- gen/test-HACKING.sh
+++ gen/test-HACKING.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -x
 
 EXPECTED=$(grep 'sha256sum:' HACKING)
 EXPECTED=$(echo -e "${EXPECTED##*sha256sum: }" | tr -d '[[:space:]]')
 
 TREE=$(find . -type d \! -path './vendor/*' \! -path './.*' | sort)
-echo -e "$TREE" | xxd
 
 ACTUAL=$(echo -e "$TREE" | sha256sum)
 ACTUAL=$(echo -e "${ACTUAL%% -}" | tr -d '[[:space:]]')
