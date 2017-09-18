bfc51602f041d0beecd5d3a4bd1fff9c6ee2730d
telyn <telyn@bytemark.co.uk>
2017-09-18 11:41:32 +0100

    Output tree before calculating sha256sum

--- gen/test-HACKING.sh
+++ gen/test-HACKING.sh
@@ -4,7 +4,10 @@ set -x
 EXPECTED=$(grep 'sha256sum:' HACKING)
 EXPECTED=$(echo -e "${EXPECTED##*sha256sum: }" | tr -d '[[:space:]]')
 
-ACTUAL=$(tree -dI 'vendor' | sha256sum)
+TREE=$(tree -dI 'vendor')
+echo -e "$TREE"
+
+ACTUAL=$(echo -e "$TREE" | sha256sum)
 ACTUAL=$(echo -e "${ACTUAL%% -}" | tr -d '[[:space:]]')
 
 [ "$EXPECTED" == "$ACTUAL" ]
