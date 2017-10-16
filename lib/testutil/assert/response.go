af5b3b052807087e215990ccdb33740cb4382469
telyn <telyn@bytemark.co.uk>
2017-10-16 14:55:22 +0100

    Fix assert.Response

--- lib/testutil/assert/response.go
+++ lib/testutil/assert/response.go
@@ -8,6 +8,6 @@ import (
 // Response asserts that response deepequals expected
 func Response(t *testing.T, testName string, response interface{}, expected interface{}) {
 	if !reflect.DeepEqual(expected, response) {
-		t.Errorf("%s - unexpected response.\nExpected: %#v\nActual: %#v", expected, response)
+		t.Errorf("%s - unexpected response.\nExpected: %#v\nActual: %#v", testName, expected, response)
 	}
 }
