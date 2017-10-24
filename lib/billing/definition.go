ba8b15995afb83f1585541dcbc3a3b286af9e4a4
telyn <telyn@bytemark.co.uk>
2017-10-24 14:59:08 +0100

    add DefaultFields and PrettyPrint to billing.Definition

--- lib/billing/definition.go
+++ lib/billing/definition.go
@@ -1,5 +1,12 @@
 package billing
 
+import (
+	"io"
+
+	"github.com/BytemarkHosting/bytemark-client/lib/output"
+	"github.com/BytemarkHosting/bytemark-client/lib/output/prettyprint"
+)
+
 // Definition is an admin-modifiable parameter for bmbilling
 // examples include account-opening credit amount and trial length
 type Definition struct {
@@ -8,4 +15,24 @@ type Definition struct {
 	Value string `json:"value"`
 	// Which auth group a user must be in to update the definition
 	UpdateGroupReq string `json:"update_group_req,omitempty"`
+}
+
+// DefaultFields returns the default fields used for making tables of Definitions
+func (d Definition) DefaultFields(f output.Format) string {
+	return "Name, Value, UpdateGroupReq"
+}
+
+// PrettyPrint writes the Definition in a human-readable form at the given detail level to wr
+func (d Definition) PrettyPrint(wr io.Writer, detail prettyprint.DetailLevel) {
+	definitionTpl := `
+		{{ define "definition_sgl" }}{{ .Name }}: {{ .Value }}{{ end }}
+		{{ define "definition_medium" }}{{ template "definition_sgl" . }}{{ end }}
+		{{ define "definition_full" -}}
+ID: {{ .ID }}
+Name: {{ .Name }}
+Value: {{ .Value }}
+Update Group Requirement: {{ .UpdateGroupReq }}
+		{{- end }}
+	`
+	prettyprint.Run(wr, definitionTpl, "definition"+detail)
 }
