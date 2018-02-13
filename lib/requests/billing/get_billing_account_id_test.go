2efbaf8a662e1556ac01f7b92650579c6b19fa92
bryanwagg <bryanwagg@icloud.com>
2018-02-13 17:14:02 +0000

    fixed expression

--- lib/requests/billing/get_billing_account_id_test.go
+++ lib/requests/billing/get_billing_account_id_test.go
@@ -1,55 +1,55 @@
 package billing_test
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/BytemarkHosting/bytemark-client/lib"
-  "github.com/BytemarkHosting/bytemark-client/lib/billing"
-  billingMethods "github.com/BytemarkHosting/bytemark-client/lib/requests/billing"
-  "github.com/BytemarkHosting/bytemark-client/lib/testutil"
-  "github.com/BytemarkHosting/bytemark-client/lib/testutil/assert"
+	"github.com/BytemarkHosting/bytemark-client/lib"
+	"github.com/BytemarkHosting/bytemark-client/lib/billing"
+	billingMethods "github.com/BytemarkHosting/bytemark-client/lib/requests/billing"
+	"github.com/BytemarkHosting/bytemark-client/lib/testutil"
+	"github.com/BytemarkHosting/bytemark-client/lib/testutil/assert"
 )
 
 func TestGetBillingAccountID(t *testing.T) {
-  tests := []struct {
-    body      []billing.DefferedStatus{}
-    username  string
-    shouldErr bool
-  }{
-    {
-      body:      []billing.DefferedStatus{},
-      username:  "bwagg",
-      shouldErr: true,
-    },
-    {
-      body: []billing.DefferedStatus{{
-        ID:        101,
-        Deffered: false,
-      }},
-      username: "bwagg",
-      shouldErr: false,
-    },
-  }
+	tests := []struct {
+		body      []billing.DefferedStatus
+		username  string
+		shouldErr bool
+	}{
+		{
+			body:      []billing.DefferedStatus{},
+			username:  "bwagg",
+			shouldErr: true,
+		},
+		{
+			body: []billing.DefferedStatus{{
+				ID:       101,
+				Deffered: false,
+			}},
+			username:  "bwagg",
+			shouldErr: false,
+		},
+	}
 
-  for i, test := range tests {
-    testName := testutil.Name(i)
-    rts := testutil.RequestTestSpec{
-      Method:        "GET",
-      Endpoint:      lib.BillingEndpoint,
-      URL:           "/api/v1/accounts/status?username=%s",
-      Response:      test.body,
-      AssertRequest: assert.QueryValue("username", test.username),
-    }
-    rts.Run(t, testutil.Name(i), true, func(client lib.Client) {
-      account, err := billingMethods.GetBillingAccountID(client, test.username)
-      if len(test.body) > 0 {
-        assert.Equal(t, testutil.Name(i), test.body[0], account)
-      }
-      if test.shouldErr {
-        assert.NotEqual(t, testName, nil, err)
-      } else {
-        assert.Equal(t, testName, nil, err)
-      }
-    })
-  }
+	for i, test := range tests {
+		testName := testutil.Name(i)
+		rts := testutil.RequestTestSpec{
+			Method:        "GET",
+			Endpoint:      lib.BillingEndpoint,
+			URL:           "/api/v1/accounts/status?username=%s",
+			Response:      test.body,
+			AssertRequest: assert.QueryValue("username", test.username),
+		}
+		rts.Run(t, testutil.Name(i), true, func(client lib.Client) {
+			account, err := billingMethods.GetBillingAccountID(client, test.username)
+			if len(test.body) > 0 {
+				assert.Equal(t, testutil.Name(i), test.body[0], account)
+			}
+			if test.shouldErr {
+				assert.NotEqual(t, testName, nil, err)
+			} else {
+				assert.Equal(t, testName, nil, err)
+			}
+		})
+	}
 }
