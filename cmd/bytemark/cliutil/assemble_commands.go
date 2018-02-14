dfba6f454d19e60569b05d20d3133dfc9cecbaff
bryanwagg <bryanwagg@icloud.com>
2018-02-14 10:11:39 +0000

    added another comment for AssembleCommands exported function

--- cmd/bytemark/cliutil/assemble_commands.go
+++ cmd/bytemark/cliutil/assemble_commands.go
@@ -4,6 +4,7 @@ import (
 	"github.com/urfave/cli"
 )
 
+// AssembleCommands slices of different command sets, merges and returns them as one full list
 func AssembleCommands(commandSlices ...[]cli.Command) (allCommands []cli.Command) {
 	for _, commands := range commandSlices {
 		allCommands = MergeCommands(allCommands, commands)
