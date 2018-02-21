dd99106077f9f68e2749946e97d38a2fc335ed8c
bryanwagg <bryanwagg@icloud.com>
2018-02-21 16:13:28 +0000

    removed the request for defered status as requests for an account is now done via big account name

--- lib/billing/deffered_status.go
+++
@@ -1,8 +0,0 @@
-package billing
-
-// DeferredStatus represents the ID and Deferred status of an account that is returned from bmbilling and
-// at the moment, we are only intrested in the ID of this, as it converts a username into a billingID.
-type DeferredStatus struct {
-	ID       int  `json:"id,omitempty"`
-	Deferred bool `json:"deferred,omitempty"`
-}
