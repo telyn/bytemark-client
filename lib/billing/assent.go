c8d848fd63e31a391bff55a49567b3261ba48e55
bryanwagg <bryanwagg@icloud.com>
2018-02-26 12:13:26 +0000

    aded json objects keys to assent struct

--- lib/billing/assent.go
+++ lib/billing/assent.go
@@ -2,10 +2,10 @@ package billing
 
 // Assent represents an assent by a person on behalf of an account to an agreement
 type Assent struct {
-	AgreementID string
-	AccountID   int
-	PersonID    int
+	AgreementID string `json:"-"`
+	AccountID   int    `json:"account_id"`
+	PersonID    int    `json:"person_id"`
 	// Name is the full name of the person
-	Name  string
-	Email string
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
