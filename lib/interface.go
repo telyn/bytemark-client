104371e9465af052c79d684da283637b36ff9ee8
bryanwagg <bryanwagg@icloud.com>
2018-03-07 14:56:03 +0000

    remove the start vm with appliance call from the interface and vm calls

--- lib/interface.go
+++ lib/interface.go
@@ -186,10 +186,6 @@ type Client interface {
 	// returns nil on success or an error otherwise.
 	StartVirtualMachine(name VirtualMachineName) (err error)
 
-	// StartVirtualMachineWithAppliance starts the named virtual machine with the named appliance.
-	// returns nil on success or an error otherwise.
-	StartVirtualMachineWithAppliance(vmName VirtualMachineName, applianceName string) (err error)
-
 	// StopVirtualMachine starts the named virtual machine.
 	// returns nil on success or an error otherwise.
 	StopVirtualMachine(name VirtualMachineName) (err error)
