f1867276dd446c8d6cf4cfbc38b9178a7e7b2e3e
James F. Carter <james.carter@bytemark.co.uk>
2018-03-22 14:33:43 +0000

    sprinkle some comments

--- lib/brain/migration_job.go
+++ lib/brain/migration_job.go
@@ -1,15 +1,19 @@
 package brain
 
+// MigrationJobQueue is a list of disc IDs that are still to be migrated as
+// part of a migration job.
 type MigrationJobQueue struct {
 	Discs []int `json:"discs,omitempty"`
 }
 
+// MigrationLocations represents source or target locations for a migration job.
 type MigrationLocations struct {
 	Discs []string `json:"discs,omitempty"`
 	Pools []string `json:"pools,omitempty"`
 	Tails []string `json:"tails,omitempty"`
 }
 
+// MigrationOptions represents options on a migration job.
 type MigrationOptions struct {
 	Priority int `json:"priority,omitempty"`
 }
@@ -21,10 +25,13 @@ type MigrationDiscStatus struct {
 	Skipped   []int `json:"skipped,omitempty"`
 }
 
+// MigrationStatus captures the status of a migration job, currently only
+// discs.
 type MigrationStatus struct {
 	Discs MigrationDiscStatus `json:"discs,omitempty"`
 }
 
+// MigrationJob is a representation of a migration job.
 type MigrationJob struct {
 	ID           int                `json:"id,omitempty"`
 	Queue        MigrationJobQueue  `json:"queue,omitempty"`
