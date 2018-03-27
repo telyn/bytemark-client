830fab67c085e848a5380f51a9a7712a74925204
bryanwagg <bryanwagg@icloud.com>
2018-03-27 13:57:57 +0100

    update the converter

--- cmd/bytemark/commands/admin/util_funcs.go
+++ cmd/bytemark/commands/admin/util_funcs.go
@@ -1,11 +1,11 @@
 package admin
 
-import "encoding/json"
+import "github.com/BytemarkHosting/bytemark-client/lib/util"
 
-func stringsToJSONNumbers(in []string) (out []json.Number) {
-	out = make([]json.Number, len(in))
+func stringsToJSONNumbers(in []string) (out []util.NumberOrString) {
+	out = make([]util.NumberOrString, len(in))
 	for i, str := range in {
-		out[i] = json.Number(str)
+		out[i] = util.NumberOrString(str)
 	}
 
 	return out
