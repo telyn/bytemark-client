b1fe29ca14bac7c3d3650779d66852814f4bea1a
James F. Carter <james.carter@bytemark.co.uk>
2018-03-27 12:47:36 +0100

    gofmt

---
+++ lib/requests/brain/create_migration_job.go
@@ -0,0 +1,15 @@
+package brain
+
+import (
+	"github.com/BytemarkHosting/bytemark-client/lib"
+	"github.com/BytemarkHosting/bytemark-client/lib/brain"
+)
+
+// CreateMigrationJob creates a new migration job as per the supplied
+// specification, returning the newly created job on success or an error
+// otherwise.
+func CreateMigrationJob(client lib.Client, mjs brain.MigrationJobSpec) (mj brain.MigrationJob, err error) {
+	r, err := client.BuildRequest("POST", lib.BrainEndpoint, "/admin/migration_jobs")
+	_, _, err = req.MarshalAndRun(mjs, &mj)
+	return
+}
