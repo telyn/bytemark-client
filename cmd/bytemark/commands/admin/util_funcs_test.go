547597084a359dc6c93f2d65b23a044c7a643e57
James F. Carter <james.carter@bytemark.co.uk>
2018-03-27 13:54:08 +0100

    replace stringsToJsonNumbers utility test with stringsToNumberOrStrings

--- cmd/bytemark/commands/admin/util_funcs_test.go
+++ cmd/bytemark/commands/admin/util_funcs_test.go
@@ -2,9 +2,9 @@ package admin
 
 import "testing"
 
-func TestStringsToJsonNumbers(t *testing.T) {
+func TestStringsToNumberOrStrings(t *testing.T) {
 	in := []string{"hello", "world", "8092"}
-	out := stringsToJsonNumbers(in)
+	out := stringsToNumberOrStrings(in)
 	if len(out) != 3 {
 		t.Fatalf("len(out) = %d, expected 3", len(out))
 	}
