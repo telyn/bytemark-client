8d92fad5fa220bf4b9b1c67d0fa0b6a90307f13a
James F. Carter <james.carter@bytemark.co.uk>
2018-03-29 11:36:26 +0100

    add brain.Migrations for representing lists of active migrations

---
+++ lib/brain/migrations_test.go
@@ -0,0 +1,50 @@
+package brain
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/BytemarkHosting/bytemark-client/lib/output/prettyprint"
+)
+
+func TestFormatMigrations(t *testing.T) {
+	tests := []struct {
+		name   string
+		in     Migrations
+		detail prettyprint.DetailLevel
+		exp    string
+	}{
+		{
+			name:   "NoMigrations",
+			in:     Migrations{},
+			detail: prettyprint.Full,
+			exp:    ``,
+		},
+		{
+			name: "OneMigration",
+			in: Migrations{{
+				ID:             123,
+				DiscID:         1,
+				TailID:         2,
+				Port:           3000,
+				CreatedAt:      "2018-03-29T08:54:50.198Z",
+				UpdatedAt:      "2018-03-29T08:54:50.198Z",
+				MigrationJobID: 77,
+			}},
+			detail: prettyprint.Full,
+			exp:    ` ▸ 123 disc: 1` + "\n",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			b := new(bytes.Buffer)
+			err := test.in.PrettyPrint(b, test.detail)
+			if err != nil {
+				t.Error(err)
+			}
+			if b.String() != test.exp {
+				t.Errorf("unexpected output: %s", b.String())
+			}
+		})
+	}
+}
