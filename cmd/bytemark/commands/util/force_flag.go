f62cfc9b56e891580b3dda53a087edce605af8d3
James F. Carter <james.carter@bytemark.co.uk>
2018-04-04 15:20:05 +0100

    lint

--- cmd/bytemark/commands/util/force_flag.go
+++ cmd/bytemark/commands/util/force_flag.go
@@ -4,7 +4,7 @@ import (
 	"github.com/urfave/cli"
 )
 
-// forceFlag is common to a bunch of commands and can have a generic Usage.
+// ForceFlag is common to a bunch of commands and can have a generic Usage.
 var ForceFlag = cli.BoolFlag{
 	Name:  "force",
 	Usage: "Do not prompt for confirmation when destroying data or increasing costs.",
