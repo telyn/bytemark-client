d7c19713af12c7f43fabaf010ea5238cff29e661
bryanwagg <bryanwagg@icloud.com>
2018-04-04 14:19:02 +0100

    because we need these things defined somewhere...

---
+++ cmd/bytemark/commands/add/add_test.go
@@ -0,0 +1,9 @@
+package add_tests
+
+import (
+	"github.com/BytemarkHosting/bytemark-client/lib"
+	"github.com/BytemarkHosting/bytemark-client/lib/brain"
+)
+
+var DefVM = lib.VirtualMachineName{Group: "default", Account: "default-account"}
+var DefGroup = lib.GroupName{Group: "default", Account: "default-account"}
