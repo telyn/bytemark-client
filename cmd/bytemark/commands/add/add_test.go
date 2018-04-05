a5e94f880535c234faed1b4e4d310702293006c1
bryanwagg <bryanwagg@icloud.com>
2018-04-05 09:32:24 +0100

    remove old file

--- cmd/bytemark/commands/add/add_test.go
+++
@@ -1,8 +0,0 @@
-package add_test
-
-import (
-	"github.com/BytemarkHosting/bytemark-client/lib"
-)
-
-var defVM = lib.VirtualMachineName{Group: "default", Account: "default-account"}
-var defGroup = lib.GroupName{Group: "default", Account: "default-account"}
