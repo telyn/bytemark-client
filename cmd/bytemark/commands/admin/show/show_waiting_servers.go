bb4926a4bed2dbe2dd8a4a4985aeeafe6235ed32
bryanwagg <bryanwagg@icloud.com>
2018-04-06 11:39:26 +0100

    move show stopped eligible vms to show waiting servers

---
+++ cmd/bytemark/commands/admin/show/show_waiting_servers.go
@@ -0,0 +1,24 @@
+package show
+
+import (
+	"github.com/BytemarkHosting/bytemark-client/cmd/bytemark/app"
+	"github.com/BytemarkHosting/bytemark-client/cmd/bytemark/app/with"
+	"github.com/BytemarkHosting/bytemark-client/lib/output"
+	"github.com/urfave/cli"
+)
+
+func init() {
+	Commands = append(Commands, cli.Command{
+		Name:      "stopped eligible vms",
+		Usage:     "shows a list of stopped VMs that should be running",
+		UsageText: "--admin show stopped_eligible_vms [--json]",
+		Flags:     app.OutputFlags("servers", "array"),
+		Action: app.Action(with.Auth, func(c *app.Context) error {
+			vms, err := c.Client().GetStoppedEligibleVMs()
+			if err != nil {
+				return err
+			}
+			return c.OutputInDesiredForm(vms, output.Table)
+		}),
+	})
+}
