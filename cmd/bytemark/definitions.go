704190924a87c74fca632d00efa683d27cc36b37
bryanwagg <bryanwagg@icloud.com>
2018-04-06 09:22:01 +0100

    move storage to the show commands package

--- cmd/bytemark/definitions.go
+++ cmd/bytemark/definitions.go
@@ -9,16 +9,6 @@ import (
 
 func init() {
 	commands = append(commands, cli.Command{
-		Name:      "hwprofiles",
-		Usage:     "list hardware profiles available for cloud servers",
-		UsageText: "hwprofiles [--json]",
-		Description: `Hardware profiles are used by cloud servers and choosing between them can be thought of as 'which virtual motherboard should I use?'.
-Generally bytemark provide two - virtio and compatibility. The virtio one has better performance but may not work with obscure operating systems, or without drivers (particularly important if you are installing windows from CD rather than our images`,
-		Flags: app.OutputFlags("hardware profiles", "array"),
-		Action: app.Action(with.Definitions, func(c *app.Context) error {
-			return c.OutputInDesiredForm(c.Definitions.HardwareProfileDefinitions(), output.List)
-		}),
-	}, cli.Command{
 		Name:        "images",
 		Aliases:     []string{"distributions", "distros"},
 		Usage:       "list images available for installation on all servers",
@@ -27,15 +17,6 @@ Generally bytemark provide two - virtio and compatibility. The virtio one has be
 		Flags:       app.OutputFlags("images", "array"),
 		Action: app.Action(with.Definitions, func(c *app.Context) error {
 			return c.OutputInDesiredForm(c.Definitions.DistributionDefinitions(), output.List)
-		}),
-	}, cli.Command{
-		Name:        "storage",
-		Usage:       "list available storage grades for cloud servers",
-		UsageText:   "storage",
-		Description: "This outputs the available storage grades for cloud servers.",
-		Flags:       app.OutputFlags("storage grades", "array"),
-		Action: app.Action(with.Definitions, func(c *app.Context) error {
-			return c.OutputInDesiredForm(c.Definitions.StorageGradeDefinitions(), output.List)
 		}),
 	}, cli.Command{
 		Name:        "zones",
