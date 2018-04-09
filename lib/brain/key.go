6520bbe8d22d0443332f0de926d605f90ea70e01
telyn <telyn@bytemark.co.uk>
2018-04-09 17:07:28 +0100

    Add brainKey and brain.Keys types

---
+++ lib/brain/key.go
@@ -0,0 +1,72 @@
+package brain
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/BytemarkHosting/bytemark-client/lib/output"
+	"github.com/BytemarkHosting/bytemark-client/lib/output/prettyprint"
+)
+
+// Key is an SSH public key which can be output nicely in a table.
+type Key struct {
+	Key string
+}
+
+// DefaultFields returns the list of default fields to feed to github.com/BytemarkHosting/row.From for this type.
+func (k Key) DefaultFields(f output.Format) string {
+	return "Key"
+}
+
+// PrettyPrint writes the key to the given writer
+func (k Key) PrettyPrint(wr io.Writer, detail prettyprint.DetailLevel) error {
+	_, err := fmt.Fprint(wr, k.Key)
+	return err
+}
+
+func (k *Key) UnmarshalText(text []byte) error {
+	k.Key = string(text)
+	return nil
+}
+
+// String returns this key as a simple string.
+func (k Key) String() string {
+	return k.Key
+}
+
+type Keys []Key
+
+// DefaultFields returns the list of default fields to feed to github.com/BytemarkHosting/row.From for this type.
+func (k Keys) DefaultFields(f output.Format) string {
+	return "Key"
+}
+
+// PrettyPrint outputs the keys, one per line, with no indent.
+func (k Keys) PrettyPrint(wr io.Writer, detail prettyprint.DetailLevel) (err error) {
+	for _, key := range k {
+		_, err = fmt.Fprintln(wr, key.Key)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (k *Keys) UnmarshalText(text []byte) error {
+	for _, line := range strings.Split(string(text), "\n") {
+		*k = append(*k, Key{Key: line})
+	}
+	return nil
+}
+
+func (k Keys) strings() (strs []string) {
+	for _, key := range k {
+		strs = append(strs, key.Key)
+	}
+	return
+}
+
+func (k Keys) MarshalText() ([]byte, error) {
+	return []byte(strings.Join(k.strings(), "\n")), nil
+}
--- lib/brain/user.go
+++ lib/brain/user.go
@@ -1,43 +1,17 @@
 package brain
 
 import (
-	"encoding/json"
 	"io"
-	"strings"
 
 	"github.com/BytemarkHosting/bytemark-client/lib/output"
 	"github.com/BytemarkHosting/bytemark-client/lib/output/prettyprint"
 )
 
-// JSONUser is used as an intermediate type that gets processed into a User. It should not have been exported.
-type JSONUser struct {
-	Username       string `json:"username"`
-	Email          string `json:"email"`
-	AuthorizedKeys string `json:"authorized_keys"`
-
-	// passwords are handled by auth these days
-	//Password       string `json:"password"`
-
-	// "users can be created (using POST) without authentication. If the
-	// request has no authentication, it will also accept an account_name
-	// parameter and create an account at the same time."
-	// this is almost certainly never going to be useful
-	//AccountName string `json:"account_name"`
-}
-
-// User turns this JSONUser into a User.
-func (jsonUser *JSONUser) User() (user User) {
-	user.Username = jsonUser.Username
-	user.Email = jsonUser.Email
-	user.AuthorizedKeys = strings.Split(jsonUser.AuthorizedKeys, "\n")
-	return
-}
-
 // User represents a Bytemark user.
 type User struct {
 	Username       string
 	Email          string
-	AuthorizedKeys []string
+	AuthorizedKeys Keys
 }
 
 // DefaultFields returns the list of default fields to feed to github.com/BytemarkHosting/row.From for this type.
@@ -58,13 +32,4 @@ Authorized keys:
 {{ end }}
 `
 	return prettyprint.Run(wr, userTpl, "user"+string(detail), user)
-}
-
-// MarshalJSON marshals the User into a JSON bytestream.
-func (user *User) MarshalJSON() ([]byte, error) {
-	return json.Marshal(&JSONUser{
-		Username:       user.Username,
-		Email:          user.Email,
-		AuthorizedKeys: strings.Join(user.AuthorizedKeys, "\n"),
-	})
 }
