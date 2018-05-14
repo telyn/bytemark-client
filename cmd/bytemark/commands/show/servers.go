0510018ceaee4c7ce22190edeb3b756ddd0752d3
telyn <telyn@bytemark.co.uk>
2018-05-14 19:44:07 +0100

    make show servers take a group name instead of account

--- cmd/bytemark/commands/show/servers.go
+++ cmd/bytemark/commands/show/servers.go
@@ -1,6 +1,8 @@
 package show
 
 import (
+	"errors"
+
 	"github.com/BytemarkHosting/bytemark-client/cmd/bytemark/app"
 	"github.com/BytemarkHosting/bytemark-client/cmd/bytemark/app/args"
 	"github.com/BytemarkHosting/bytemark-client/cmd/bytemark/app/with"
@@ -13,23 +15,39 @@ func init() {
 	Commands = append(Commands, cli.Command{
 		Name:      "servers",
 		Usage:     "show all the servers in an account",
-		UsageText: "show servers [account]",
+		UsageText: "show servers [--group <group> | --account <account>] [group]",
 		Description: `This command shows all the servers in the given account, or in your default account if not specified.
-Deleted servers are included in the list, with ' (deleted)' appended.`,
+Deleted servers are included in the list, with ' (deleted)' appended.
+
+If --group and --account are specified, the group will be displayed and the account will be ignored.`,
 		Flags: append(app.OutputFlags("servers", "array"),
 			cli.GenericFlag{
+				Name:  "group",
+				Usage: "the group to list the servers of",
+				Value: new(app.GroupNameFlag),
+			},
+			cli.StringFlag{
 				Name:  "account",
-				Usage: "the account to list the servers of",
-				Value: new(app.AccountNameFlag),
+				Usage: "the account to show all the servers of",
 			},
 		),
-		Action: app.Action(args.Optional("account"), with.Account("account"), with.Auth, func(c *app.Context) error {
+		Action: app.Action(args.Optional("group"), with.Auth, func(c *app.Context) error {
 			servers := brain.VirtualMachines{}
-
-			for _, g := range c.Account.Groups {
-				servers = append(servers, g.VirtualMachines...)
+			if c.IsSet("group") {
+				groupName := c.GroupName("group")
+				group, err := c.Client().GetGroup(groupName)
+				if err != nil {
+					return err
+				}
+				return c.OutputInDesiredForm(brain.VirtualMachines(group.VirtualMachines), output.List)
 			}
-			return c.OutputInDesiredForm(servers, output.List)
+			if c.IsSet("account") {
+				for _, g := range c.Account.Groups {
+					servers = append(servers, g.VirtualMachines...)
+				}
+				return c.OutputInDesiredForm(servers, output.List)
+			}
+			return errors.New("A group or account must be specified")
 		}),
 	})
 }
