591fa105c359cca78494e6fa9a1f91c5009b51d6
Hannah Pirie <hannah.pirie@bytemark.co.uk>
2018-08-20 14:41:09 +0100

    add test for GetServersOnHead

---
+++ lib/requests/brain/get_dependant_servers_test.go
@@ -0,0 +1,47 @@
+package brain_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/BytemarkHosting/bytemark-client/lib"
+	"github.com/BytemarkHosting/bytemark-client/lib/brain"
+	brainMethods "github.com/BytemarkHosting/bytemark-client/lib/requests/brain"
+	"github.com/BytemarkHosting/bytemark-client/lib/testutil"
+	"github.com/BytemarkHosting/bytemark-client/lib/testutil/assert"
+)
+
+func TestGetServersOnHead(t *testing.T) {
+	testName := testutil.Name(0)
+
+	testServers := brain.VirtualMachines{{
+		Cores:					4,
+		Memory:					4,
+		Name:					"test_server",
+		ID:                		123,
+		Hostname:				"test_hostname",
+		Head:					"test_head",
+	}}
+
+	rts := testutil.RequestTestSpec{
+		Method:        "GET",
+		URL:           "/admin/heads/123/virtual_machines",
+		Endpoint:      lib.BrainEndpoint,
+		Response: json.RawMessage(`[{
+		"Cores": 4,
+		"Memory": 4,
+		"Name": "test_server",
+		"ID": 123,
+		"Hostname": "test_hostname",
+		"Head": "test_head"
+	    }]`),
+	}
+
+	rts.Run(t, testName, true, func(client lib.Client) {
+		servers, err := brainMethods.GetServersOnHead(client, "123", "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, testName, servers, testServers)
+	})
+}
