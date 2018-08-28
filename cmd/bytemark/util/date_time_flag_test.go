f5fe067dda0a10ae7b5d7a662566c99def33fc5f
Hannah Pirie <hannah.pirie@bytemark.co.uk>
2018-08-28 14:55:57 +0100

    update datetime flag tests

--- cmd/bytemark/util/date_time_flag_test.go
+++ cmd/bytemark/util/date_time_flag_test.go
@@ -9,16 +9,49 @@ import (
 
 func TestDateTimeFlag(t *testing.T) {
 	is := is.New(t)
+	tests := []struct {
+		input  string
+		layout string
+	}{
+		{
+			input:  "1/2/2018 15:30:00 +01:00",
+			layout: "2006-01-02T15:04:05-07:00",
+		},
+		{
+			input:  "15:30:00",
+			layout: "T15:04:05",
+		},
+		{
+			input:  "1/2/2018 15:30",
+			layout: "2006-01-02T15:04",
+		},
+		{
+			input:  "1-2-2018",
+			layout: "2006-01-02",
+		},
+		{
+			input:  "1st June 2018",
+			layout: "2006-01-02",
+		},
+		{
+			input:  "June 1 2018",
+			layout: "2006-01-02",
+		},
+	}
 
-	var flag DateTimeFlag
+	for _, test := range tests {
+		t.Run("datetime flag", func(t *testing.T) {
+			var flag DateTimeFlag
 
-	err := flag.Set("1/2/2018 3pm")
+			err := flag.Set(test.input)
 
-	is.Nil(err)
-	is.OK(flag.String())
+			is.Nil(err)
+			is.OK(flag.String())
 
-	// check format
-	formatted, err := time.Parse("2006-01-02T15:04:05-0700", flag.String())
-	is.Nil(err)
-	is.OK(formatted)
+			// check format
+			formatted, err := time.Parse(test.layout, flag.String())
+			is.Nil(err)
+			is.OK(formatted)
+		})
+	}
 }
