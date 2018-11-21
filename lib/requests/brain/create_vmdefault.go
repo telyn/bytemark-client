7a59a35dcb791439b7f18edb2d2d82aad954c372
Tom Stirling <thomas.stirling@bytemark.co.uk>
2018-11-21 10:11:03 +0000

    refactored create_vmdefault

--- lib/requests/brain/create_vmdefault.go
+++ lib/requests/brain/create_vmdefault.go
@@ -8,8 +8,8 @@ import (
 )
 
 // CreateVMDefault creates a new VM Default with the specified parameters,
-// returning the newly created job on success or an error otherwise.
-func CreateVMDefault(client lib.Client, name string, public bool, serverSettings brain.VMDefaultSpec) error {
+// returning the newly created VM Default on success or an error otherwise.
+func CreateVMDefault(client lib.Client, name string, public bool, serverSettings brain.VMDefaultSpec) (err error) {
 	if name == "" {
 		return fmt.Errorf("VMDefault must have a non-blank name")
 	}
@@ -17,9 +17,9 @@ func CreateVMDefault(client lib.Client, name string, public bool, serverSettings
 		return fmt.Errorf("VM must have a non-blank name")
 	}
 
-	r, err := client.BuildRequest("POST", lib.BrainEndpoint, "/vm_defaults")
+	req, err := client.BuildRequest("POST", lib.BrainEndpoint, "/vm_defaults")
 	if err != nil {
-		return err
+		return
 	}
 
 	obj := map[string]interface{}{
@@ -28,6 +28,6 @@ func CreateVMDefault(client lib.Client, name string, public bool, serverSettings
 		"server_settings": serverSettings,
 	}
 
-	_, _, err = r.MarshalAndRun(obj, nil)
-	return err
+	_, _, err = req.MarshalAndRun(obj, nil)
+	return
 }
