8e7b33e61f9393434d671302acc7eda03b769981
telyn <telyn@bytemark.co.uk>
2018-12-10 12:02:03 +0000

    Change the way slice flags are generated, refactor list_types into its own package

--- cmd/bytemark/app/flags/group_name_slice.go
+++ cmd/bytemark/app/flags/group_name_slice.go
@@ -1,33 +1,55 @@
 package flags
 
+// This file was automatically generate using
+// cmd/bytemark/app/flags/gen/slice_flags - do not edit it by hand!
+
 import (
-	"flag"
+	"strings"
 
 	"github.com/BytemarkHosting/bytemark-client/cmd/bytemark/app"
-	"github.com/BytemarkHosting/bytemark-client/lib"
 )
 
-// GroupNameSliceFlag is for any --group flags that can be specified multiple
-// times
-type GroupNameSliceFlag interface {
-	flag.Value
-	app.Preprocesser
-	GroupNames() []lib.GroupName
-}
+// GroupNameSliceFlag is used for GroupNameFlags that may be specified more than
+// once. It's a slice of GroupNameFlag in order to avoid rewriting parsing
+// logic.
+type GroupNameSliceFlag []GroupNameFlag
 
-type groupNameSliceFlag struct {
-	GenericSliceFlag
-}
-
-// NewGroupNameSliceFlag creates a new
-func NewGroupNameSliceFlag() GroupNameSliceFlag {
-	return groupNameSliceFlag{
-		template: GroupNameFlag{},
+func (sf *GroupNameSliceFlag) Preprocess(ctx *app.Context) error {
+	for i := range *sf {
+		err := (*sf)[i].Preprocess(ctx)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-// GroupNames returns the GroupNameSlice for which this flag is named
-func (gnsf GroupNameSliceFlag) GroupNames() (groupNames []lib.GroupName) {
-	gnsf.copyValues(groupNames)
-	return
+
+// Set appends a GroupNameFlag (created for you) to the slice
+func (sf *GroupNameSliceFlag) Set(value string) error {
+	flag := GroupNameFlag{}
+	err := flag.Set(value)
+	if err != nil {
+		return err
+	}
+	*sf = append(*sf, flag)
+	return nil
+}
+
+// String returns all values in the slice, comma-delimeted
+func (sf GroupNameSliceFlag) String() string {
+	strs := make([]string, len(sf))
+	for i, value := range sf {
+		strs[i] = value.String()
+	}
+	return strings.Join(strs, ", ")
+}
+
+// GroupNameSlice returns the named flag as a GroupNameSliceFlag,
+// if it was one in the first place.
+func GroupNameSlice(ctx *app.Context, name string) GroupNameSliceFlag {
+	if sf, ok := ctx.Context.Generic(name).(GroupNameSliceFlag); ok {
+		return sf	
+	}
+	return GroupNameSliceFlag{}
 }
