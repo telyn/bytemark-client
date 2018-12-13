4bf30220f16c90a7228b9b5af29c9a02130d6bfe
telyn <telyn@bytemark.co.uk>
2018-12-13 11:38:16 +0000

    add some output to test-HACKING.sh

--- gen/test-HACKING.sh
+++ gen/test-HACKING.sh
@@ -8,4 +8,9 @@ TREE=$(find . -type d \! -path './vendor/*' \! -path './.*' | sort)
 ACTUAL=$(echo -e "$TREE" | sha256sum)
 ACTUAL=$(echo -e "${ACTUAL%% -}" | tr -d '[[:space:]]')
 
+echo "$TREE"
+echo
+echo "sha256sum: $ACTUAL"
+echo "HACKING sha256sum: $EXPECTED"
+
 [ "$EXPECTED" == "$ACTUAL" ]
